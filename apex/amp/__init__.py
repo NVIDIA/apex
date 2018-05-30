6d30e1ffde3ec1b7b93e14432bb05eb39f0b58bc
Carl Case <carlc@nvidia.com>
2018-05-30 13:37:34 -0700

    rename build->init

--- apex/amp/__init__.py
+++ apex/amp/__init__.py
@@ -1,2 +1,2 @@
-from .amp import build, half_function, float_function, promote_function,\
+from .amp import init, half_function, float_function, promote_function,\
     register_half_function, register_float_function, register_promote_function
