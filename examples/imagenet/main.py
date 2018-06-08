06ee98c2779010330bc4d7108ac104d9f2df50d6
Christian Sarofeen <csarofeen@nvidia.com>
2018-06-08 21:14:04 +0000

    [Imagenet example] Switch validation to same I/O pipeline so validation produces correct result.

--- examples/imagenet/main.py
+++ examples/imagenet/main.py
@@ -190,19 +190,14 @@ def main():
         train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler, collate_fn=fast_collate)
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-    
-    
     val_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder(valdir, transforms.Compose([
             transforms.Resize(val_size),
             transforms.CenterCrop(crop_size),
-            transforms.ToTensor(),
-            normalize,
         ])),
         batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+        num_workers=args.workers, pin_memory=True,
+        collate_fn=fast_collate)
 
     if args.evaluate:
         validate(val_loader, model, criterion)
