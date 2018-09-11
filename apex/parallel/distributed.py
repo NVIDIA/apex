7ca571a53770a7455109c60975d27255989f03c2
Michael Carilli <mcarilli@nvidia.com>
2018-09-11 13:31:51 -0700

    ready to test more robust mapping method

--- apex/parallel/distributed.py
+++ apex/parallel/distributed.py
@@ -134,7 +134,7 @@ class DistributedDataParallel(Module):
         self.message_size = message_size
         
         # reference to last iterations parameters to see if anything has changed
-        self.param_refs = []
+        self.active_params = []
         
         self.reduction_stream = torch.cuda.Stream()
         
@@ -172,6 +172,7 @@ class DistributedDataParallel(Module):
                 t_record = torch.cuda.IntTensor(self.record)
                 dist.broadcast(t_record, 0)
                 self.record = [int(entry) for entry in t_record]
+                self.param_id_to_record_i = {id(self.active_params[a]) : i for i, a in enumerate(self.record)}
                 self.needs_refresh = False
 
             grads = [param.grad.data for param in self.module.parameters() if param.grad is not None]
@@ -184,7 +185,7 @@ class DistributedDataParallel(Module):
 
             grads = []
             for i in range(self.ready_end, len(self.param_state)):
-                param = self.param_refs[self.record[i]]
+                param = self.active_params[self.record[i]]
                 if param.grad is not None:
                     grads.append(param.grad.data)
             grads = [param.grad.data for param in self.ready_params] + grads
@@ -197,48 +198,41 @@ class DistributedDataParallel(Module):
                     
             torch.cuda.current_stream().wait_stream(self.reduction_stream)
 
-        for param_i, param in enumerate([p for p in self.module.parameters() if p.requires_grad]):
-            def wrapper(param_i):
-                
+        for param in self.module.parameters() if param.requires_grad:
+            def wrapper(param):
                 def allreduce_hook(*unused):
                     if self.needs_refresh:
-                        self.record.append(param_i)
+                        self.record.append(param_id_to_active_i[id(param)])
                         Variable._execution_engine.queue_callback(allreduce_params)
                     else:
                         Variable._execution_engine.queue_callback(flush_buckets)
-                        self.comm_ready_buckets(self.record.index(param_i))
-                    
+                        self.comm_ready_buckets(param_id_to_record_i[id(param_i)])
                     
                 if param.requires_grad:
                     param.register_hook(allreduce_hook)
-            wrapper(param_i)
+
+            wrapper(param)
 
 
-    def comm_ready_buckets(self, param_ind):
+    def comm_ready_buckets(self, record_i):
 
-        if self.param_state[param_ind] != 0:
+        if self.param_state[record_i] != 0:
             raise RuntimeError("Error: Your model uses shared parameters, DDP flag shared_params must be set to True in initialization.")
             
         
         if self.param_state[self.ready_end] == 0:
-            self.param_state[param_ind] = 1
+            self.param_state[record_i] = 1
             return
 
 
         while self.ready_end < len(self.param_state) and self.param_state[self.ready_end] == 1:
-            try:
-                self.ready_params.append(self.param_refs[self.record[self.ready_end]])
-            except:
-                print("\n\nself.ready_end = {}".format(self.ready_end))
-                print("len(self.record) = {}".format(len(self.record))) 
-                print("record[ready_end] = {}\n\n".format(self.record[self.ready_end]))
-                raise
+            self.ready_params.append(self.active_params[self.record[self.ready_end]])
             self.ready_numel += self.ready_params[-1].numel()
             self.ready_end += 1
 
 
         if self.ready_numel < self.message_size:
-            self.param_state[param_ind] = 1
+            self.param_state[record_i] = 1
             return
             
         grads = [param.grad.data for param in self.ready_params]
@@ -266,7 +260,7 @@ class DistributedDataParallel(Module):
                 self.param_state[i] = 2
                 self.ready_params.pop(0)
 
-        self.param_state[param_ind] = 1
+        self.param_state[record_i] = 1
         
     def forward(self, *inputs, **kwargs):
 
@@ -276,21 +270,18 @@ class DistributedDataParallel(Module):
         # Forward has the authority to set needs_refresh to True, but only allreduce_params
         # in backward has the authority to set needs_refresh to False.
         # Parentheses are not necessary for correct order of operations, but make the intent clearer.
-        if ( (not self.param_refs) or 
+        if ( (not self.active_params) or 
              self.shared_param or 
-             (len(param_list) != len(self.param_refs)) or
-             any([param1 is not param2 for param1, param2 in zip(param_list, self.param_refs)]) ):
+             (len(param_list) != len(self.active_params)) or
+             any([param1 is not param2 for param1, param2 in zip(param_list, self.active_params)]) ):
             self.needs_refresh = True
-
-        print("len(param_list) = ", len(param_list))
-        print("len(parameters()) = ", len(list(self.module.parameters())))
-        print("needs_refresh = ", self.needs_refresh)
 
         if self.needs_refresh:
             self.record = []
+            self.param_id_to_active_i = {id(param) : i for i, param in enumerate(param_list)}  
             
         self.param_state = [0 for i in range(len(param_list))]
-        self.param_refs = param_list
+        self.active_params = param_list
         self.needs_reduction = True
 
         self.ready_start = 0
