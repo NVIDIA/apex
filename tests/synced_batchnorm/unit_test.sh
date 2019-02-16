598fbc88a5be485db73ee0cbfba53925a8bc7c9e
root <ekrimer@nvidia.com>
2019-02-16 00:05:42 +0000

    adding test_groups.py to unit_test.sh

--- tests/synced_batchnorm/unit_test.sh
+++ tests/synced_batchnorm/unit_test.sh
@@ -1,3 +1,5 @@
 python single_gpu_unit_test.py
 python -m torch.distributed.launch --nproc_per_node=2 two_gpu_unit_test.py
 python -m torch.distributed.launch --nproc_per_node=2 two_gpu_unit_test.py --fp64
+#beware, you need a system with at least 4 gpus to test group_size<world_size
+python -m torch.distributed.launch --nproc_per_node=4 test_groups.py --group_size=2
