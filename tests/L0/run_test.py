6d40465a7a90a9e07d00cecbb17faced04a19ce6
Lam Dang <tunglam.dang@gmail.com>
2019-04-10 20:09:51 +0000

    add new tests to run_test.py

--- tests/L0/run_test.py
+++ tests/L0/run_test.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 
-test_dirs = ["run_amp", "run_fp16util", "run_mixed_adam"]
+test_dirs = ["run_amp", "run_fp16util", "run_mixed_adam", "run_fused_layer_norm"]
 
 runner = unittest.TextTestRunner(verbosity=2)
 
