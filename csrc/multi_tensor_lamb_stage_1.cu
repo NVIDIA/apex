dfac69d97c230206600e5c5337a53f3b4718c973
Thor Johnsen <tjohnsen@nvidia.com>
2019-05-30 15:35:52 -0700

    Bug fix

--- csrc/multi_tensor_lamb_stage_1.cu
+++ csrc/multi_tensor_lamb_stage_1.cu
@@ -95,7 +95,7 @@ void multi_tensor_lamb_stage1_cuda(
 {
   using namespace at;
 
-  float clipped_global_grad_norm = global_grad_norm > 1.0f ? global_grad_norm : 1.0f;
+  float clipped_global_grad_norm = global_grad_norm < 1.0f ? global_grad_norm : 1.0f;
   float next_step = float(step+1);
   float beta1_correction = 1.0f - std::pow(beta1, next_step);
   float beta2_correction = 1.0f - std::pow(beta2, next_step);
