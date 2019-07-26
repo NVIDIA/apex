6be1c61633469213bfc19481b555fc2edba2b861
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2019-07-25 21:49:04 -0700

    Add directory

--- tests/L1/cross_product/run.sh
+++ tests/L1/cross_product/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-DATADIR="/home/mcarilli/Desktop/pt18data/apex_stale/examples/imagenet/bare_metal_train_val/"
-# DATADIR="/opt/home/apex/examples/imagenet/"
+if [ ! -d "val" ]; then
+   ln -sf /data/imagenet/val-jpeg/ val
+fi
+if [ ! -d "train" ]; then
+   ln -sf /data/imagenet/train-jpeg/ train
+fi
+
+DATADIR="./"
 cp ../common/* .
 bash run_test.sh single_gpu $1 $DATADIR yes
--- tests/L1/cross_product_distributed/run.sh
+++ tests/L1/cross_product_distributed/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+if [ ! -d "val" ]; then
+   ln -sf /data/imagenet/val-jpeg/ val
+fi
+if [ ! -d "train" ]; then
+   ln -sf /data/imagenet/train-jpeg/ train
+fi
+
+DATADIR="./"
 cp ../common/* .
-bash run_test.sh distributed $1
+bash run_test.sh distributed $DATADIR
