d4fdc98fce7b19e5c483d4630796b7e899a8369b
Deyu Fu <deyuf@nvidia.com>
2019-10-03 11:43:55 -0700

    remove unnecessary try import

--- apex/contrib/optimizers/__init__.py
+++ apex/contrib/optimizers/__init__.py
@@ -1,10 +1,2 @@
 from .fp16_optimizer import FP16_Optimizer
-try:
-    import torch
-    import fused_adam_cuda
-    from .fused_adam import FusedAdam
-    del torch
-    del fused_adam_cuda
-    del fused_adam
-except ImportError as err:
-    print("apex was installed without --deprecated_fused_adam flag, contrib.fused_adam is not available")
+from .fused_adam import FusedAdam
