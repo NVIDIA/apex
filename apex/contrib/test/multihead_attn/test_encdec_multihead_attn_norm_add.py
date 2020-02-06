3f94528e9d25938e72dcdeea3d6217efce6c6607
Kevin Stephano <kevin.stephano@gmail.com>
2020-02-06 12:02:12 -0800

    Add Fast Multihead Attention to APEX Contrib (#697)
    * Adding C++ Multihead Attention implementation to contrib.
    
    * Add reference test that at least works for forward.
    
    * Remove CublasLt support from multihead attention.
    
    * Add new Python version of self attention.
    
    * Update python model of MHA with backward pass.
    
    * Fixed Output Linear connection in MHA.
    
    * Clean up compiles and add documentation to PySelfAttention.
    
    * Add Encdec Python version of multihead attention.  Cleanup files.
    
    * Tests for self and encdec multihead attention.
    
    * Add reference pytorch implementation of attention with norm and add.
    
    * Add cutlass branch definition.
    
    * Add cutlass download to compile.
    
    * Add norm/add tests.
    
    * Add biases to pytorch python versions.
    
    * Add tests and fix issues with python version of attention masking.
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update perf test parameters.
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Fix matmul1 output tensor size.  Fix tests that missed issue.

---
+++ apex/contrib/test/multihead_attn/test_encdec_multihead_attn_norm_add.py
@@ -0,0 +1,77 @@
+import torch
+
+import unittest
+
+from apex.contrib.multihead_attn import EncdecMultiheadAttn
+
+class EncdecMultiheadAttnNormAddTest(unittest.TestCase):
+    def setUp(self, seed=1234):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+        self.seq_length   = 80
+        self.sequences    = 10
+        self.hidden_dim   = 1024
+        self.heads        = 16
+        self.dropout_prob = 0.0
+
+        self.ref_layer = EncdecMultiheadAttn(self.hidden_dim, 
+                                             self.heads, 
+                                             dropout=self.dropout_prob, 
+                                             bias=False, 
+                                             include_norm_add=True, 
+                                             impl='default')
+        self.ref_layer.cuda().half()
+        self.ref_layer.reset_parameters()
+        self.ref_inputs_q = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
+                                        dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
+        self.ref_inputs_k = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
+                                        dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
+
+        # Reset seed so parameters are identical
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        
+        self.tst_layer = EncdecMultiheadAttn(self.hidden_dim, 
+                                             self.heads, 
+                                             dropout=self.dropout_prob, 
+                                             bias=False, 
+                                             include_norm_add=True, 
+                                             impl='fast')
+        self.tst_layer.cuda().half()
+        self.tst_layer.reset_parameters()
+        
+        self.tst_inputs_q = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
+                                        dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
+        self.tst_inputs_k = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
+                                        dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
+
+    def test_encdec_multihead_attn_norm_add(self) :
+        grads         = torch.randn_like(self.tst_inputs_q)
+
+        ref_outputs,_ = self.ref_layer.forward(self.ref_inputs_q, 
+                                               self.ref_inputs_k, 
+                                               self.ref_inputs_k,
+                                               key_padding_mask=None, 
+                                               need_weights=False, 
+                                               attn_mask=None,
+                                               is_training=True)
+
+        tst_outputs,_ = self.tst_layer.forward(self.tst_inputs_q, 
+                                               self.tst_inputs_k, 
+                                               self.tst_inputs_k,
+                                               key_padding_mask=None, 
+                                               need_weights=False, 
+                                               attn_mask=None,
+                                               is_training=True)
+        
+        self.ref_inputs_q.backward(grads)
+        self.tst_inputs_q.backward(grads)
+
+        self.assertTrue(torch.allclose(self.ref_inputs_q,  self.tst_inputs_q,  atol=1e-5, rtol=1e-5))
+        self.assertTrue(torch.allclose(self.ref_inputs_k,  self.tst_inputs_k,  atol=1e-5, rtol=1e-5))
+        self.assertTrue(torch.allclose(ref_outputs, tst_outputs, atol=1e-3, rtol=1e-3))
+        self.assertTrue(torch.allclose(self.ref_inputs_q.grad, self.tst_inputs_q.grad, atol=1e-3, rtol=1e-3))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ apex/contrib/test/multihead_attn/test_self_multihead_attn_norm_add.py
@@ -0,0 +1,72 @@
+import torch
+
+import unittest
+
+from apex.contrib.multihead_attn import SelfMultiheadAttn
+
+class SelfMultiheadAttnNormAddTest(unittest.TestCase):
+    def setUp(self, seed=1234):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+        self.seq_length   = 80
+        self.sequences    = 10
+        self.hidden_dim   = 1024
+        self.heads        = 16
+        self.dropout_prob = 0.0
+
+        self.ref_layer = SelfMultiheadAttn(self.hidden_dim, 
+                                           self.heads, 
+                                           dropout=self.dropout_prob, 
+                                           bias=False, 
+                                           include_norm_add=True, 
+                                           impl='default')
+        self.ref_layer.cuda().half()
+        self.ref_layer.reset_parameters()
+        self.ref_inputs = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
+                                      dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
+
+        # Reset seed so parameters are identical
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        
+        self.tst_layer = SelfMultiheadAttn(self.hidden_dim, 
+                                           self.heads, 
+                                           dropout=self.dropout_prob, 
+                                           bias=False, 
+                                           include_norm_add=True, 
+                                           impl='fast')
+        self.tst_layer.cuda().half()
+        self.tst_layer.reset_parameters()
+        
+        self.tst_inputs = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
+                                      dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
+
+    def test_self_multihead_attn_norm_add(self) :
+        grads         = torch.randn_like(self.tst_inputs)
+
+        ref_outputs,_ = self.ref_layer.forward(self.ref_inputs, 
+                                               self.ref_inputs, 
+                                               self.ref_inputs,
+                                               key_padding_mask=None, 
+                                               need_weights=False, 
+                                               attn_mask=None,
+                                               is_training=True)
+
+        tst_outputs,_ = self.tst_layer.forward(self.tst_inputs, 
+                                               self.tst_inputs, 
+                                               self.tst_inputs,
+                                               key_padding_mask=None, 
+                                               need_weights=False, 
+                                               attn_mask=None,
+                                               is_training=True)
+        
+        self.ref_inputs.backward(grads)
+        self.tst_inputs.backward(grads)
+
+        self.assertTrue(torch.allclose(self.ref_inputs,  self.tst_inputs,  atol=1e-5, rtol=1e-5))
+        self.assertTrue(torch.allclose(ref_outputs, tst_outputs, atol=1e-3, rtol=1e-3))
+        self.assertTrue(torch.allclose(self.ref_inputs.grad, self.tst_inputs.grad, atol=1e-3, rtol=1e-3))
+
+if __name__ == '__main__':
+    unittest.main()
