90729bc8d68f8c63624d16601fd89dcce58d1651
Kexin Yu <kexinznzn@gmail.com>
2020-03-31 21:38:33 -0700

    fix parameter type

--- apex/contrib/csrc/optimizers/fused_lamb_cuda_kernel.cu
+++ apex/contrib/csrc/optimizers/fused_lamb_cuda_kernel.cu
@@ -41,7 +41,7 @@ struct LAMBStage1Functor
     const float epsilon,
     adamMode_t mode,
     const float decay,
-    float* global_grad_norm,
+    float global_grad_norm,
     float max_global_grad_norm)
   {
     // I'd like this kernel to propagate infs/nans.
@@ -52,7 +52,7 @@ struct LAMBStage1Functor
     int chunk_idx = tl.block_to_chunk[blockIdx.x];
     int n = tl.sizes[tensor_loc];
 
-    float clipped_global_grad_norm = (*global_grad_norm) > max_global_grad_norm ? (*global_grad_norm) / max_global_grad_norm : 1.0f;
+    float clipped_global_grad_norm = global_grad_norm > max_global_grad_norm ? global_grad_norm / max_global_grad_norm : 1.0f;
 
     T* g = (T*)tl.addresses[0][tensor_loc];
     g += chunk_idx*chunk_size;
