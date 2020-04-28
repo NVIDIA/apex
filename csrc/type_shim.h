8124df1369ca927305ea4de0e8ffff9d083e6eb2
Chaitanya Sri Krishna Lolla <lollachaitanya@gmail.com>
2020-04-28 10:09:18 -0700

    Enable Apex on ROCm and support multi tensor support. (#1)
    * Initial commit to hipify all cuda code
    
    * enable multi_tensor_apply extension
    
    * added generatedFileCleaner to handle nested hip files

--- csrc/type_shim.h
+++ csrc/type_shim.h
@@ -115,8 +115,13 @@ __device__ __forceinline__ T reduce_block_into_lanes
     // __SYNCWARP();
 
     #pragma unroll
-    for(int i = 16; i >= lanes; i >>= 1)
+    for(int i = 16; i >= lanes; i >>= 1) {
+#ifdef __HIP_PLATFORM_HCC__
+      final = final + __shfl_down(0xffffffff, final, i);
+#else
       final = final + __shfl_down_sync(0xffffffff, final, i);
+#endif
+    }
   }
 
   if(share_result)
@@ -165,8 +170,13 @@ __device__ __forceinline__ T reduce_block_into_lanes_max_op
     // __SYNCWARP();
 
     #pragma unroll
-    for(int i = 16; i >= lanes; i >>= 1)
+    for(int i = 16; i >= lanes; i >>= 1) {
+#ifdef __HIP_PLATFORM_HCC__
+      final = fmaxf(fabsf(final), fabsf(__shfl_down(0xffffffff, final, i)));
+#else
       final = fmaxf(fabsf(final), fabsf(__shfl_down_sync(0xffffffff, final, i)));
+#endif
+    }
   }
 
   if(share_result)
