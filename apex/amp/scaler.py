3ccdd63dd66bc171c464d57426301f16599b4d6e
Chaitanya Sri Krishna Lolla <lollachaitanya@gmail.com>
2020-05-07 13:13:47 -0700

    enable python only base sparse tensor support for loss scaling (#2)

--- apex/amp/scaler.py
+++ apex/amp/scaler.py
@@ -6,12 +6,18 @@ from itertools import product
 def scale_check_overflow_python(model_grad, master_grad, scale, check_overflow=False):
     # Exception handling for 18.04 compatibility
     if check_overflow:
-        cpu_sum = float(model_grad.float().sum())
+        if model_grad.is_sparse:
+            cpu_sum = float(model_grad.float()._values().sum())
+        else:
+            cpu_sum = float(model_grad.float().sum())
         if cpu_sum == float('inf') or cpu_sum == -float('inf') or cpu_sum != cpu_sum:
             return True
 
     if master_grad is not model_grad: # copy_ probably internally short-circuits this
-        master_grad.copy_(model_grad)
+        if model_grad.is_sparse:
+            master_grad.copy_(model_grad.to_dense())
+        else:
+            master_grad.copy_(model_grad)
     if scale != 1.0:
         master_grad.mul_(scale)
     return False
@@ -19,7 +25,10 @@ def scale_check_overflow_python(model_grad, master_grad, scale, check_overflow=F
 def axpby_check_overflow_python(model_grad, stashed_grad, master_grad, a, b, check_overflow=False):
     # Exception handling for 18.04 compatibility
     if check_overflow:
-        cpu_sum = float(model_grad.float().sum())
+        if model_grad.is_sparse:
+            cpu_sum = float(model_grad.float()._values().sum())
+        else:
+            cpu_sum = float(model_grad.float().sum())
         if cpu_sum == float('inf') or cpu_sum == -float('inf') or cpu_sum != cpu_sum:
             return True
 
