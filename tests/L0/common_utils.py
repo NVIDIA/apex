a73d7d3b4a0f2afe5d4bf8d702c9180c712f3832
lcskrishna <lollachaitanya@gmail.com>
2020-05-19 00:08:42 +0000

    create a base framework for adding tests

---
+++ tests/L0/common_utils.py
@@ -0,0 +1,17 @@
+'''
+This file contains common utility functions for running the unit tests on ROCM.
+'''
+
+import torch
+
+TEST_WITH_ROCM = os.getenv('APEX_TEST_WITH_ROCM', '0') == '1'
+
+## Wrapper to skip the unit tests.
+def skipIfRocm(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if TEST_WITH_ROCM:
+            raise unittest.SkipTest("test doesn't currently work on ROCm stack.")
+        else:
+            fn(*args, **kwargs)
+    return wrapper
