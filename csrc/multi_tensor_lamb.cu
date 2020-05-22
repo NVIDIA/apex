3a727a01f7b24595f4bf5a9333b878b04b796ef2
Kexin Yu <kexinznzn@gmail.com>
2020-05-21 21:08:30 -0700

    .data<...>()

--- csrc/multi_tensor_lamb.cu
+++ csrc/multi_tensor_lamb.cu
@@ -387,7 +387,7 @@ void multi_tensor_lamb_cuda(
         epsilon,
         (adamMode_t) mode,
         weight_decay,
-        global_grad_norm.data_ptr<float>(),
+        global_grad_norm.data<float>(),
         max_grad_norm); )
 
   // Compute update norms
