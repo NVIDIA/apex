4f27890978a449d3fb2eee6e0765657e7bad3b87
Kexin Yu <kexinznzn@gmail.com>
2020-07-17 00:05:28 -0700

    import distributed_adam_cuda

--- apex/contrib/optimizers/distributed_fused_adam_v4.py
+++ apex/contrib/optimizers/distributed_fused_adam_v4.py
@@ -53,7 +53,7 @@ class DistributedFusedAdam(torch.optim.Optimizer):
                  dwu_num_chunks=4, predivide=True, e5m2_allgather=False,
                  do_not_flatten_model=False,
                  step_supports_amp_scaling=True):
-        global fused_adam_cuda
+        global fused_adam_cuda, distributed_adam_cuda
         fused_adam_cuda = importlib.import_module("fused_adam_cuda")
         distributed_adam_cuda = importlib.import_module("distributed_adam_cuda")
 
