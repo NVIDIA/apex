9c05985c721f139ebc49d93754bac00178ba04d3
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2021-07-16 13:39:22 -0700

    local_rank and install cuda version fix

--- setup.py
+++ setup.py
@@ -98,13 +98,15 @@ def check_cuda_torch_binary_vs_bare_metal(cuda_dir):
     print("\nCompiling cuda extensions with")
     print(raw_output + "from " + cuda_dir + "/bin\n")
 
-    if (bare_metal_major != torch_binary_major) or (bare_metal_minor != torch_binary_minor):
-        raise RuntimeError("Cuda extensions are being compiled with a version of Cuda that does " +
-                           "not match the version used to compile Pytorch binaries.  " +
-                           "Pytorch binaries were compiled with Cuda {}.\n".format(torch.version.cuda) +
-                           "In some cases, a minor-version mismatch will not cause later errors:  " +
-                           "https://github.com/NVIDIA/apex/pull/323#discussion_r287021798.  "
-                           "You can try commenting out this check (at your own risk).")
+    if os.getenv('APEX_SKIP_TORCH_CUDA_VERSION_CHECK', '0') != '1':
+        if (bare_metal_major != torch_binary_major) or (bare_metal_minor != torch_binary_minor):
+            raise RuntimeError("Cuda extensions are being compiled with a version of Cuda that does " +
+                               "not match the version used to compile Pytorch binaries.  " +
+                               "Pytorch binaries were compiled with Cuda {}.\n".format(torch.version.cuda) +
+                               "In some cases, a minor-version mismatch will not cause later errors:  " +
+                               "https://github.com/NVIDIA/apex/pull/323#discussion_r287021798.  "
+                               "You can set environment variable APEX_SKIP_TORCH_CUDA_VERSION_CHECK=1 "
+                               "to skip this check (at your own risk).")
 
 
 # Set up macros for forward/backward compatibility hack around
