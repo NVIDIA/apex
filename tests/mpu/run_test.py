8b17592d7094c46c76f2424203bab757cbf795c3
Piotr Bialecki <pbialecki@nvidia.com>
2021-08-20 04:35:12 +0000

    Mkozuki/mpu repo restructure

---
+++ tests/mpu/run_test.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import sys
+
+
+if __name__ == '__main__':
+    python_executable_path = sys.executable
+    directory = os.path.dirname(os.path.abspath(__file__))
+    files = [
+        os.path.join(directory, f) for f in os.listdir(directory)
+        if f.startswith("test_") and os.path.isfile(os.path.join(directory, f))
+    ]
+    print("#######################################################")
+    print(f"# Python executable path: {python_executable_path}")
+    print(f"# {len(files)} tests: {files}")
+    print("#######################################################")
+    errors = []
+    for i, test_file in enumerate(files, 1):
+        test_run_cmd = f"NVIDIA_TF32_OVERRIDE=0  {python_executable_path} {test_file} --micro-batch-size 2 --num-layers 1 --hidden-size 256 --num-attention-heads 8 --max-position-embeddings 32 --encoder-seq-length 32 --use-cpu-initialization"  # NOQA
+        print(f"### {i} / {len(files)}: cmd: {test_run_cmd}")
+        try:
+            output = subprocess.check_output(
+                test_run_cmd, shell=True
+            ).decode(sys.stdout.encoding).strip()
+        except Exception as e:
+            errors.append((test_file, str(e)))
+        else:
+            if '>> passed the test :-)' not in output:
+                errors.append(test_file, output)
+    else:
+        if not errors:
+            print("### PASSED")
+        else:
+            print("### FAILED")
+            print(f"{len(errors)} out of {len(files)} tests failed")
+            for (filename, log) in errors:
+                print(f"File: {filename}\nLog: {log}")
---
+++ tests/mpu/test_initialize.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# import sys
+# sys.path.append("../..")
+# sys.path.append("../../..")
+import torch
+
+import apex
+from apex.mpu import global_vars
+from apex.mpu.tests.commons import print_separator
+from apex.mpu.tests.commons import initialize_distributed
+from apex.mpu.tests.commons import TEST_SUCCESS_MESSAGE
+
+
+global_vars.set_global_variables()
+
+
+def test_initialize_model_parallel(tensor_model_parallel_size):
+
+    if torch.distributed.get_rank() == 0:
+        print('> testing initialize_model_parallel with size {} ...'.format(
+            tensor_model_parallel_size))
+    tensor_model_parallel_size_ = min(
+        tensor_model_parallel_size,
+        torch.distributed.get_world_size(),
+    )
+    assert not apex.mpu.model_parallel_is_initialized()
+    apex.mpu.initialize_model_parallel(tensor_model_parallel_size_)
+    assert apex.mpu.model_parallel_is_initialized()
+
+    # Checks.
+    def check(group, world_size, rank):
+        assert world_size == torch.distributed.get_world_size(group=group)
+        assert rank == torch.distributed.get_rank(group=group)
+
+    # Model parallel.
+    world_size = tensor_model_parallel_size_
+    rank = torch.distributed.get_rank() % tensor_model_parallel_size_
+    assert world_size == apex.mpu.get_tensor_model_parallel_world_size()
+    assert rank == apex.mpu.get_tensor_model_parallel_rank()
+    check(apex.mpu.get_tensor_model_parallel_group(), world_size, rank)
+
+    # Data parallel.
+    world_size = torch.distributed.get_world_size() // tensor_model_parallel_size_
+    rank = torch.distributed.get_rank() // tensor_model_parallel_size
+    assert world_size == apex.mpu.get_data_parallel_world_size()
+    assert rank == apex.mpu.get_data_parallel_rank()
+    check(apex.mpu.get_data_parallel_group(), world_size, rank)
+
+    # Reset groups
+    apex.mpu.destroy_model_parallel()
+
+    torch.distributed.barrier()
+    if torch.distributed.get_rank() == 0:
+        print(TEST_SUCCESS_MESSAGE)
+
+
+def test_get_tensor_model_parallel_src_rank(tensor_model_parallel_size_):
+
+    if torch.distributed.get_rank() == 0:
+        print('> testing get_tensor_model_parallel_src_rank with size {} ...'.format(
+            tensor_model_parallel_size_))
+    tensor_model_parallel_size = min(
+        tensor_model_parallel_size_,
+        torch.distributed.get_world_size(),
+    )
+    assert not apex.mpu.model_parallel_is_initialized()
+    apex.mpu.initialize_model_parallel(tensor_model_parallel_size)
+    assert apex.mpu.model_parallel_is_initialized()
+
+    # Checks
+    src_rank = torch.distributed.get_rank() - apex.mpu.get_tensor_model_parallel_rank()
+    assert apex.mpu.get_tensor_model_parallel_src_rank() == src_rank
+
+    # Reset groups
+    apex.mpu.destroy_model_parallel()
+
+    torch.distributed.barrier()
+    if torch.distributed.get_rank() == 0:
+        print('>> passed the test :-)')
+
+
+if __name__ == '__main__':
+
+    initialize_distributed()
+    world_size = torch.distributed.get_world_size()
+    tensor_model_parallel_size = 1
+    while tensor_model_parallel_size <= world_size:
+        print_separator('test initialize model parallel')
+        test_initialize_model_parallel(tensor_model_parallel_size)
+        print_separator('test model parallel source rank')
+        test_get_tensor_model_parallel_src_rank(tensor_model_parallel_size)
+        tensor_model_parallel_size *= 2
