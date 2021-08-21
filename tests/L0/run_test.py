d8d625f18602855e61eab376d94f0d8d11bd9ea7
Masaki Kozuki <mkozuki@nvidia.com>
2021-08-21 09:55:33 +0900

    make more cwd agnostic

--- tests/L0/run_test.py
+++ tests/L0/run_test.py
@@ -1,20 +1,38 @@
+import os
 import unittest
 import sys
 
-test_dirs = ["run_amp", "run_fp16util", "run_optimizers", "run_fused_layer_norm", "run_pyprof_nvtx", "run_pyprof_data", "run_mlp"]
 
-runner = unittest.TextTestRunner(verbosity=2)
+TEST_ROOT = os.path.dirname(os.path.abspath(__file__))
+TEST_DIRS = [
+    "run_amp",
+    "run_fp16util",
+    "run_optimizers",
+    "run_fused_layer_norm",
+    "run_pyprof_nvtx",
+    "run_pyprof_data",
+    "run_mlp",
+    "run_mpu",
+]
 
-errcode = 0
 
-for test_dir in test_dirs:
-    suite = unittest.TestLoader().discover(test_dir)
+def main():
+    runner = unittest.TextTestRunner(verbosity=2)
+    errcode = 0
+    for test_dir in TEST_DIRS:
+        test_dir = os.path.join(TEST_ROOT, test_dir)
+        print(test_dir)
+        suite = unittest.TestLoader().discover(test_dir)
 
-    print("\nExecuting tests from " + test_dir)
+        print("\nExecuting tests from " + test_dir)
 
-    result = runner.run(suite)
+        result = runner.run(suite)
 
-    if not result.wasSuccessful():
-        errcode = 1
+        if not result.wasSuccessful():
+            errcode = 1
 
-sys.exit(errcode)
+    sys.exit(errcode)
+
+
+if __name__ == '__main__':
+    main()
