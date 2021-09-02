9320497431b30f43a40cf3f802f2e6869bcd2a53
Kexin Yu <kexinznzn@gmail.com>
2021-09-01 19:10:47 -0700

    debug

--- apex/contrib/optimizers/distributed_fused_lamb.py
+++ apex/contrib/optimizers/distributed_fused_lamb.py
@@ -587,7 +587,6 @@ class DistributedFusedLAMB(torch.optim.Optimizer):
                         l2_grad_norm_sq = self._flat_grads_shards[self._rank_in_group].norm(dtype=torch.float32, p=2)**2
                     else:
                         l2_grad_norm_sq = self._fp16_g.norm(dtype=torch.float32, p=2)**2
-                    l2_grad_norm_sq = multi_tensor_applier(self.multi_tensor_l2norm, self._overflow_buf, [self._contrib_compute_update_term_tensor_list[0]], False)[0] ** 2
                     torch.distributed.all_reduce(l2_grad_norm_sq, group=self._l2_grad_norm_pg)
                     self._L2_grad_norm = l2_grad_norm_sq.sqrt()
         else:
