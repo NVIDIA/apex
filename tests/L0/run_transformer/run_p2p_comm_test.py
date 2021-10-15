cbb3e0538d8454ed558a44cfa6977e9aba82da7d
Masaki Kozuki <mkozuki@nvidia.com>
2021-10-14 20:32:28 -0700

    simplify

--- tests/L0/run_transformer/run_p2p_comm_test.py
+++ tests/L0/run_transformer/run_p2p_comm_test.py
@@ -2,6 +2,7 @@ import torch
 
 from apex.transformer import parallel_state
 from apex.transformer.pipeline_parallel.p2p_communication import send_forward_recv_backward
+from apex.transformer.pipeline_parallel.schedules.common import rank_print
 from apex.transformer.testing import global_vars
 from apex.transformer.testing.commons import initialize_distributed
 from apex.transformer.testing.commons import print_separator
@@ -17,22 +18,18 @@ def run_test(pipeline_model_parallel_size):
     shape = [3]
     tensor = torch.full(shape, rank).cuda()
 
+    rank_print("Start `send_forward_recv_backward`")
     grad = send_forward_recv_backward(tensor, shape, dtype=torch.float)
+    rank_print("Finish `send_forward_recv_backward`")
     next_rank = parallel_state.get_pipeline_model_parallel_next_rank()
     expected = torch.ones_like(tensor) * next_rank
 
     assert torch.equal(expected, grad)
+    rank_print("Exitting `run_test` function")
 
 
 if __name__ == "__main__":
     initialize_distributed()
 
-    pipeline_model_parallel_size = 2
-    while pipeline_model_parallel_size <= torch.distributed.get_world_size():
-        try:
-            run_test(pipeline_model_parallel_size)
-        except Exception as e:
-            print_separator(str(e))
-            break
-        else:
-            pipeline_model_parallel_size *= 2
+    pipeline_model_parallel_size = torch.distributed.get_world_size()
+    run_test(pipeline_model_parallel_size)
