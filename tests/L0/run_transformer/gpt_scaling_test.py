d892a8b84b879f329d103f17bdd8e66c0b9f7fe9
riship <riship@nvidia.com>
2021-11-27 16:20:25 -0800

    framework to scale up the gpt2 test for variety of distributed setups

--- tests/L0/run_transformer/gpt_scaling_test.py
+++ tests/L0/run_transformer/gpt_scaling_test.py
@@ -8,6 +8,7 @@ import copy
 import random
 import numpy as np
 import subprocess
+from apex.transformer.testing.commons import TEST_SUCCESS_MESSAGE
 
 
 def set_seed():
@@ -18,42 +19,52 @@ def set_seed():
 
 
 def run_gpt(n):
-	cmd = 'python3 native.py ' + str(n)
 	args = list(cmd.split(' '))
-	try:
-		p = subprocess.Popen(args)
-		outs, errs = p.communicate()
-	except:
-		traceback.print_exc(file=sys.stdout)
-		print(args)
-		print("Inference failed")
-	filename = 'native' + str(n) + '.txt'
-	fileread = str(open(filename,'r').read())
-	runtime = float(fileread)
-	os.remove(filename)
-	return runtime
+	p = subprocess.Popen(args)
+	outs, errs = p.communicate(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	outs = outs.splitlines()
+	success = False
+	for out in outs:
+		if "Average Iteration Time:" in out:
+			runtime = float(out[out.find(':')+1:])
+		if "Initialized GPT-2 w/:" in out:
+			init_dict = json.loads(out[out.find(':')+1:])
+		if out == TEST_SUCCESS_MESSAGE:
+			success=True
+	return runtime, float(int(init_dict['num_params']))/10.0**9, success
 
 
 def plot(runtimes):
 	import matplotlib.pyplot as plt
-	plt.scatter(runtimes.keys(), runtimes.values())
+	for distributed_setting in runtimes.keys():
+		plt.scatter(runtimes[distributed_setting].keys(), runtimes[distributed_setting].values(), label=distributed_setting)
+	plt.legend()
 	plt.xlabel('Parameters (Billions)')
 	plt.ylabel('Training Iteration time (s)')
-	plt.title(str("Offload Foward Pass Scaling"))
-	plt.savefig('offload_scaling.png')
+	plt.title(str("GPT Scaling w/ Offloading"))
+	plt.savefig('offload_gpt_scaling.png')
 	plt.close()
 
 
 def main():
 	runtimes = {}
-	try:
+	for data_parr, tens_parr, pipe_parr in [(8,1,1), (4,2,1), (2,1,4), (1,2,4)]:
+		dist_setting = 'ddp=' + str(data_parr) + ', tensor_parr=' + str(tens_parr) + ', pipe_parr=' + str(pipe_parr)
+		runtimess[dist_setting] = {} 
+		print("Beginning Testing for", dist_setting)
 		for n in range(500,1000000,500):
-			bill_params = round((1024.0 * 1025.0 / (10.0**9)) * n,3)
-			print("Testing", n,"1024x1024 layers ->", bill_params, 'billion parameters:', end=' ')
-			runtime = run_offload(n)
-			runtimes[bill_params] = runtime
-			print(runtime, 'ms')
-	except Exception as e:
-		print(e)
-		print(runtimes)
-		plot(runtimes)
+			cmd = "WORLDSIZE=8 python3 -m torch.distributed.launch --nproc_per_node=8 run_gpt_minimal_test.py \
+			--micro-batch-size 1 --num-layers " + str(n) + " --hidden-size 128 --num-attention-heads 16 \
+			--max-position-embeddings 128 --seq-length 128 --tensor-model-parallel-size " + str(tens_parr) + \
+			" --pipeline-model-parallel-size " + str(pipe_parr)
+			print(cmd)
+			runtime, bill_params, success = run_offload(cmd)
+			if success:
+				runtimes[dist_setting][bill_params] = runtime
+				print(runtime, 'ms')
+			else:
+				print("GPT-2 w/", n, "layers failed using", dist_setting)
+				print("Moving on to the next distributed setting...")
+				break
+	print(runtimes)
+	plot(runtimes)
