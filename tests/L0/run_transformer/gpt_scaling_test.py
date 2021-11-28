d1f08b68716454c1d83d9cc18e5cce21a6dc3f4c
riship <riship@nvidia.com>
2021-11-27 18:34:09 -0800

    framework to scale up the gpt2 test for variety of distributed setups

--- tests/L0/run_transformer/gpt_scaling_test.py
+++ tests/L0/run_transformer/gpt_scaling_test.py
@@ -54,7 +54,7 @@ def main():
 		print("Beginning Testing for", dist_setting)
 		for n in range(500,1000000,500):
 			cmd = "WORLDSIZE=8 python3 -m torch.distributed.launch --nproc_per_node=8 run_gpt_minimal_test.py"
-			cmd += '--micro-batch-size 1 --num-layers " + str(n) + " --hidden-size 128 --num-attention-heads 16'
+			cmd += "--micro-batch-size 1 --num-layers " + str(n) + " --hidden-size 128 --num-attention-heads 16"
 			cmd += '--max-position-embeddings 128 --seq-length 128 --tensor-model-parallel-size ' + str(tens_parr)
 			cmd += " --pipeline-model-parallel-size " + str(pipe_parr)
 			print(cmd)
