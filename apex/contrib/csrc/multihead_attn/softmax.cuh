dfa4c9456f9422e5f53e649d9d31e08297aa5f62
Masaki Kozuki <mkozuki@nvidia.com>
2021-12-10 10:54:55 +0900

    build fixed again but undefined symbol again

--- apex/contrib/csrc/multihead_attn/softmax.cuh
+++ apex/contrib/csrc/multihead_attn/softmax.cuh
@@ -23,6 +23,57 @@ template <typename Datatype, int ELEMENTS_PER_LDG>
 __device__ __inline__ void apply_additive_mask(Datatype *dst,
                                                const Datatype *additive_mask);
 
+template <>
+__device__ __inline__ void copy_vector<__half, 1>(__half *dst,
+                                                  const __half *src) {
+  *dst = *src;
+}
+
+template <>
+__device__ __inline__ void copy_vector<float, 1>(float *dst, const float *src) {
+  *dst = *src;
+}
+
+template <>
+__device__ __inline__ void copy_vector<__half, 4>(__half *dst,
+                                                  const __half *src) {
+  *((float2 *)dst) = *((float2 *)src);
+}
+template <>
+__device__ __inline__ void copy_vector<uint8_t, 1>(uint8_t *dst,
+                                                   const uint8_t *src) {
+  *dst = *src;
+}
+
+template <>
+__device__ __inline__ void copy_vector<uint8_t, 4>(uint8_t *dst,
+                                                   const uint8_t *src) {
+  *((half2 *)dst) = *((half2 *)src);
+}
+
+template <>
+__device__ __inline__ void apply_mask<__half, 1>(__half *dst, __half value,
+                                                 const uint8_t *src) {
+  if (*src == 1) {
+    *dst = value;
+  }
+}
+
+template <>
+__device__ __inline__ void
+apply_additive_mask<__half, 1>(__half *dst, const __half *additive_mask) {
+  *dst += *additive_mask;
+}
+
+template <>
+__device__ __inline__ void
+apply_additive_mask<__half, 4>(__half *dst, const __half *additive_mask) {
+  *dst += *additive_mask;
+  *(dst + 1) += *(additive_mask + 1);
+  *(dst + 2) += *(additive_mask + 2);
+  *(dst + 3) += *(additive_mask + 3);
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Warp Softmax forward
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
