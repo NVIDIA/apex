b8fc93f773ca2460675c21651942ba328d47971f
Masaki Kozuki <mkozuki@nvidia.com>
2021-12-16 18:25:22 +0900

    fix

--- apex/_autocast_utils.py
+++ apex/_autocast_utils.py
@@ -3,10 +3,15 @@ from typing import Optional
 import torch
 
 
+_has_get_autocast_gpu_dtype_available = hasattr(torch, "get_autocast_gpu_dtype")
+
+
 def _get_current_dtype(dtype: Optional[torch.dtype] = None) -> torch.dtype:
     if not torch.is_autocast_enabled():
         return torch.float or dtype
     else:
+        if not _has_get_autocast_gpu_dtype_available:
+            return torch.half
         return torch.get_autocast_gpu_dtype()
 
 
