679caf10ad92146da7db52463c3480380a1e62f2
Masaki Kozuki <mkozuki@nvidia.com>
2022-01-24 15:30:44 -0800

    missing deallocate_pipeline_outputs

--- apex/transformer/pipeline_parallel/schedules/fwd_bwd_pipelining_without_interleaving.py
+++ apex/transformer/pipeline_parallel/schedules/fwd_bwd_pipelining_without_interleaving.py
@@ -163,6 +163,7 @@ def forward_backward_pipelining_without_interleaving(
     dtype: Optional[torch.dtype] = None,
     grad_scaler: Optional[torch.cuda.amp.GradScaler] = None,
     disable_autocast: bool = False,
+    deallocate_pipeline_outputs: bool = False,
 ) -> List[Union[torch.Tensor, Sequence[torch.Tensor]]]:
     """Run non-interleaved 1F1B schedule, with communication between pipeline stages.
 
