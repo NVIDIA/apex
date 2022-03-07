afa1dd12b6c51d76c5bc7102108cf0a0bd243d2b
Masaki Kozuki <mkozuki@nvidia.com>
2022-03-07 13:07:59 -0800

    refactor

--- tests/L0/run_transformer/test_parallel_state.py
+++ tests/L0/run_transformer/test_parallel_state.py
@@ -1,12 +1,9 @@
 import os
-import sys
 
-import torch
-from torch import distributed as dist
 from torch.testing._internal import common_utils
-from torch.testing._internal import common_distributed
 
 from apex.transformer import parallel_state
+from apex.transformer.testing.distributed_test_base import DistributedTestBase
 
 
 os.environ["BACKEND"] = "NCCL"
@@ -20,74 +17,7 @@ def calc_expected_tensor_model_paralell_rank(
     return rank % tensor_model_parallel_world_size
 
 
-class ParallelStateTest(common_distributed.MultiProcessTestCase):
-
-    BACKEND_NCCL = "nccl"
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self._world_size = min(torch.cuda.device_count(), super().world_size)
-
-    def setUp(self) -> None:
-        super().setUp()
-        self._world_size = min(torch.cuda.device_count(), super().world_size)
-        self._spawn_processes()
-
-    def tearDown(self) -> None:
-        super().tearDown()
-        self._world_size = None
-
-    # N.B. (mkozuki): From the perspective of execution time, I think
-    # faster tests are preferred to longer ones, but in some cases,
-    # we want to run longer for coverage.
-    # So, preparing a knob to set world_size > 4.
-    @property
-    def world_size(self) -> int:
-        return self._world_size
-
-    @world_size.setter
-    def world_size(self, new_world_size: int) -> None:
-        self._world_size = new_world_size
-
-    # TODO (mkozuki): Check if this is seriously needed.
-    @property
-    def init_method(self):
-        return f"{common_utils.FILE_SCHEMA}{self.file_name}"
-
-    @classmethod
-    def _run(cls, rank, test_name, file_name, pipe):
-        self = cls(test_name)
-        self.assertTrue(torch.cuda.is_available())
-        self.rank = rank
-        self.file_name = file_name
-
-        print(f"[dist init] rank = {self.rank}, world_size = {self.world_size}")
-
-        backend = os.environ.get("BACKEND", None)
-        if backend is None:
-            back_end = ParallelStateTest.BACKEND_NCCL
-
-        try:
-            dist.init_process_group(
-                init_method=self.init_method,
-                backend=backend,
-                world_size=int(self.world_size),
-                rank=self.rank,
-            )
-        except RuntimeError as e:
-            if "recompile" in e.args[0]:
-                print(f"Backend of {back_end} not available")
-                sys.exit(0)
-            raise
-
-        torch.cuda.set_device(self.rank % torch.cuda.device_count())
-
-        dist.barrier()
-        self.run_test(test_name, pipe)
-        dist.barrier()
-
-        dist.destroy_process_group()
-        sys.exit(0)
+class ParallelStateTest(DistributedTestBase):
 
     def test_initialize_model_parallel(self) -> None:
 
