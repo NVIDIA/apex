70b809e8a8994e8a8e8add1d97ccdeea7a149c9b
Masaki Kozuki <mkozuki@nvidia.com>
2022-03-19 18:02:13 -0700

    add test

--- apex/contrib/csrc/focal_loss/focal_loss_cuda.cpp
+++ apex/contrib/csrc/focal_loss/focal_loss_cuda.cpp
@@ -6,13 +6,18 @@
 // CUDA forward declarations
 
 std::vector<at::Tensor> focal_loss_forward_cuda(
-    const at::Tensor &cls_output, const at::Tensor &cls_targets_at_level,
-    const at::Tensor &num_positives_sum, const int64_t num_real_classes,
-    const float alpha, const float gamma, const float smoothing_factor);
+  const at::Tensor &cls_output,
+  const at::Tensor &cls_targets_at_level,
+  const at::Tensor &num_positives_sum,
+  const int64_t num_real_classes,
+  const float alpha,
+  const float gamma,
+  const float smoothing_factor);
 
-at::Tensor focal_loss_backward_cuda(const at::Tensor &grad_output,
-                                    const at::Tensor &partial_grad,
-                                    const at::Tensor &num_positives_sum);
+at::Tensor focal_loss_backward_cuda(
+  const at::Tensor &grad_output,
+  const at::Tensor &partial_grad,
+  const at::Tensor &num_positives_sum);
 
 // C++ interface
 
@@ -24,21 +29,33 @@ at::Tensor focal_loss_backward_cuda(const at::Tensor &grad_output,
   CHECK_CONTIGUOUS(x)
 
 std::vector<at::Tensor> focal_loss_forward(
-    const at::Tensor &cls_output, const at::Tensor &cls_targets_at_level,
-    const at::Tensor &num_positives_sum, const int64_t num_real_classes,
-    const float alpha, const float gamma, const float smoothing_factor) {
+  const at::Tensor &cls_output,
+  const at::Tensor &cls_targets_at_level,
+  const at::Tensor &num_positives_sum,
+  const int64_t num_real_classes,
+  const float alpha,
+  const float gamma,
+  const float smoothing_factor
+) {
   CHECK_INPUT(cls_output);
   CHECK_INPUT(cls_targets_at_level);
   CHECK_INPUT(num_positives_sum);
 
-  return focal_loss_forward_cuda(cls_output, cls_targets_at_level,
-                                 num_positives_sum, num_real_classes, alpha,
-                                 gamma, smoothing_factor);
+  return focal_loss_forward_cuda(
+    cls_output,
+    cls_targets_at_level,
+    num_positives_sum,
+    num_real_classes,
+    alpha,
+    gamma,
+    smoothing_factor);
 }
 
-at::Tensor focal_loss_backward(const at::Tensor &grad_output,
-                               const at::Tensor &partial_grad,
-                               const at::Tensor &num_positives_sum) {
+at::Tensor focal_loss_backward(
+  const at::Tensor &grad_output,
+  const at::Tensor &partial_grad,
+  const at::Tensor &num_positives_sum
+) {
   CHECK_INPUT(grad_output);
   CHECK_INPUT(partial_grad);
 
