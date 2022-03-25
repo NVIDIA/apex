cd8db094428225d543364488f26331ab92d76004
Thor Johnsen <tjohnsen@nvidia.com>
2022-03-25 09:49:11 -0700

    Forgot

--- apex/contrib/bottleneck/__init__.py
+++ apex/contrib/bottleneck/__init__.py
@@ -1,2 +1,2 @@
 from .bottleneck import Bottleneck, SpatialBottleneck
-from .halo_exchangers import HaloExchangerNoComm, HaloExchangerAllGather, HaloExchangerSendRecv
+from .halo_exchangers import HaloExchangerNoComm, HaloExchangerAllGather, HaloExchangerSendRecv, HaloExchangerPeer
