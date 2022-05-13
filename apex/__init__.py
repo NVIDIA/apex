d36397d2b8ce5c8854997e4ec2828e056e8fda89
Masaki Kozuki <mkozuki@nvidia.com>
2022-05-13 09:03:04 -0700

    Change the timing of deprecation warning (#1383)

--- apex/__init__.py
+++ apex/__init__.py
@@ -20,7 +20,7 @@ from . import fp16_utils
 # load time) the error message is timely and visible.
 from . import optimizers
 from . import normalization
-from . import pyprof
+# from . import pyprof
 from . import transformer
 
 
