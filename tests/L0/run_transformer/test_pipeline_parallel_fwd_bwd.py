514e144e244a4944ce241d0bf061e63fa8bca849
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2022-05-18 13:30:58 -0400

    add grad check

--- tests/L0/run_transformer/test_pipeline_parallel_fwd_bwd.py
+++ tests/L0/run_transformer/test_pipeline_parallel_fwd_bwd.py
@@ -31,33 +31,39 @@ logging.getLogger("apex").setLevel(logging.WARNING)
 
 weight_coeff = 1024
 
-@torch.no_grad()
-def init_weights(m):
-    rank = torch.distributed.get_rank()
-    if isinstance(m, torch.nn.Linear):
-        m.weight.fill_((rank + 1.0) / weight_coeff)
-        m.bias.fill_(1.0)
+def get_init_weights_func(offset: int = 0):
+    @torch.no_grad()
+    def init_weights(m):
+        rank = parallel_state.get_pipeline_model_parallel_rank()
+        if isinstance(m, torch.nn.Linear):
+            m.weight.fill_((rank + offset + 1.0) / weight_coeff)
+            m.bias.fill_(1.0)
+    return init_weights
 
 
-def get_target_loss(hidden_size: int, microbatch_size: int, parallel_model_world_size: int, world_size: int) -> float:  
-    layers_per_rank = world_size // parallel_model_world_size
-    data = torch.arange(start = 0, end = layers_per_rank, dtype = torch.int) + 1
+def get_target_loss(global_batch_shape: tuple, hidden_size: int, total_layers: int) -> float:  
+    model = []
+    data = torch.ones(global_batch_shape, dtype=torch.float)
+    for i in range(total_layers):
+        w = torch.ones((hidden_size, hidden_size)) * (i + 1.0) / weight_coeff
+        b = torch.ones(hidden_size)
 
-    w = (torch.arange(world_size, dtype = torch.float) + 1.0) / weight_coeff
-    b = torch.ones(world_size, dtype = torch.int)
-    w = hidden_size * w
+        w.requires_grad_()
+        b.requires_grad_()
 
-    for s_id in range(0, world_size, layers_per_rank):
-        e_id = s_id+layers_per_rank
-        data = w[s_id:e_id] * data + b[s_id:e_id]
+        # don't need to care about transpose semantics as all values are the same
+        data = torch.matmul(w, data) + b
+        model.append([w, b])
 
-    return hidden_size * hidden_size * torch.sum(data).item() * microbatch_size / layers_per_rank
+    loss = data.sum() / global_batch_shape[0]
+    loss.backward()
 
+    return float((data.sum()/global_batch_shape[0]).item()), model
 
 class PipelineParallelForwardBackwardTest(DistributedTestBase):
 
-    GLOBAL_BATCH_SIZE = 16
-    MICRO_BATCH_SIZE = 2
+    GLOBAL_BATCH_SIZE = 32
+    MICRO_BATCH_SIZE = 1
     HIDDEN_SIZE = 32
 
     @property
@@ -72,6 +78,10 @@ class PipelineParallelForwardBackwardTest(DistributedTestBase):
         virtual_pipeline_model_parallel_size: Optional[int],
         async_comm: bool = False,
     ) -> None:
+        if fwd_bwd_func == _forward_backward_pipelining_with_interleaving:
+            assert virtual_pipeline_model_parallel_size is not None
+            assert virtual_pipeline_model_parallel_size > 1
+
         for dtype, deallocate_pipeline_outputs in itertools.product(
             [torch.float32] + _get_autocast_dtypes(), (True, False),
         ):
@@ -108,7 +118,9 @@ class PipelineParallelForwardBackwardTest(DistributedTestBase):
                 PipelineParallelForwardBackwardTest.HIDDEN_SIZE,
             )
 
-            batch =(((self.rank + 1) * torch.ones(global_batch_shape)).cuda(), )
+            batch = None
+            if parallel_state.is_pipeline_first_stage:
+                batch = (torch.ones(global_batch_shape, dtype=torch.float).cuda(), )
 
             model = build_model(
                 testing_utils.model_provider_func,
@@ -117,8 +129,10 @@ class PipelineParallelForwardBackwardTest(DistributedTestBase):
                 hidden_size=PipelineParallelForwardBackwardTest.HIDDEN_SIZE,
             )
 
-            for model_module in model:
-                model_module.apply(init_weights)
+            
+            offset = pipeline_model_parallel_world_size if virtual_pipeline_model_parallel_size is not None else 0
+            for idx, model_module in enumerate(model):
+                model_module.apply(get_init_weights_func(idx*offset))
 
             _param_groups = _get_params_for_weight_decay_optimization(model)
             optimizer = torch.optim.Adam(_param_groups, lr=1e-3)
@@ -145,11 +159,26 @@ class PipelineParallelForwardBackwardTest(DistributedTestBase):
             if dtype == torch.float32:
                 hidden_size = PipelineParallelForwardBackwardTest.HIDDEN_SIZE
                 microbatch_size = PipelineParallelForwardBackwardTest.MICRO_BATCH_SIZE
-                target_loss = get_target_loss(hidden_size, microbatch_size, pipeline_model_parallel_world_size, self.world_size)
-
+                total_layers = pipeline_model_parallel_world_size
+                if virtual_pipeline_model_parallel_size is not None:
+                    total_layers *= virtual_pipeline_model_parallel_size
+                target_loss, target_model = get_target_loss(global_batch_shape, hidden_size, total_layers)
                 for loss_item in loss:
                     x = loss_item['avg']
-                    torch.testing.assert_close(x, target_loss*torch.ones_like(x))
+                    torch.testing.assert_close(x / microbatch_size, target_loss*torch.ones_like(x))
+
+                if not forward_only:
+                    for vm_id, model_module in enumerate(model):
+                        params = list(model_module.parameters())
+                        rank = params[0].get_device()
+                        offset = pipeline_model_parallel_world_size
+                        param_id = (rank * offset) // self.world_size + vm_id * offset
+                        target_params = target_model[param_id]
+
+                        torch.testing.assert_close(params[0].cpu(), target_params[0])
+                        torch.testing.assert_close(params[1].cpu(), target_params[1])
+                        torch.testing.assert_close(params[0].grad.cpu() / microbatch_size, target_params[0].grad)
+                        torch.testing.assert_close(params[1].grad.cpu() / microbatch_size, target_params[1].grad)
 
             if not forward_only:
                 for m in model:
@@ -168,34 +197,33 @@ class PipelineParallelForwardBackwardTest(DistributedTestBase):
 
     def test_pipelining_default(self):
         self._forward_backward_test_impl(
-            False, forward_backward_pipelining_without_interleaving, None, None
+            False, forward_backward_pipelining_without_interleaving, 8, None
         )
 
     def test_pipelining_async(self):
         self._forward_backward_test_impl(
-            False, forward_backward_pipelining_without_interleaving, None, None, async_comm=True
+            False, forward_backward_pipelining_without_interleaving, 8, None, async_comm=True
         )
 
     def test_pipelining_inference(self):
         self._forward_backward_test_impl(
-            True, forward_backward_pipelining_without_interleaving, None, None
+            True, forward_backward_pipelining_without_interleaving, 8, None
         )
 
     def test_pipelining_inference_async(self):
         self._forward_backward_test_impl(
-            True, forward_backward_pipelining_without_interleaving, None, None, async_comm=True
+            True, forward_backward_pipelining_without_interleaving, 8, None, async_comm=True
         )
 
     def test_pipelining_with_interleaving(self):
         self._forward_backward_test_impl(
-            False, _forward_backward_pipelining_with_interleaving, None, None
+            False, _forward_backward_pipelining_with_interleaving, 8, virtual_pipeline_model_parallel_size=4
         )
 
     def test_pipelining_with_interleaving_inference(self):
         self._forward_backward_test_impl(
-            True, _forward_backward_pipelining_with_interleaving, None, None
+            True, _forward_backward_pipelining_with_interleaving, 8, virtual_pipeline_model_parallel_size=4
         )
-
 
 if __name__ == "__main__":
     common_utils.run_tests()
