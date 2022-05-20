d6eb82ef955e15dd8d30751682927fe55824ed56
Masaki Kozuki <mkozuki@nvidia.com>
2022-05-20 14:21:20 -0700

    add layernorm with sequence_parallel_enabled attr

---
+++ apex/transformer/layers/__init__.py
@@ -0,0 +1,8 @@
+from apex.transformer.layers.layer_norm import FastLayerNorm
+from apex.transformer.layers.layer_norm import FusedLayerNorm
+
+
+__all__ = [
+    "FastLayerNorm",
+    "FusedLayerNorm",
+]
