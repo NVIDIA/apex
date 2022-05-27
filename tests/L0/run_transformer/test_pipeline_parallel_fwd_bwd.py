26614b48208737b24c29f9d52840ac746c6df974
Eddie Yan <eddiey@nvidia.com>
2022-05-27 04:43:39 +0000

    check in

--- tests/L0/run_transformer/test_pipeline_parallel_fwd_bwd.py
+++ tests/L0/run_transformer/test_pipeline_parallel_fwd_bwd.py
@@ -50,10 +50,10 @@ def get_init_weights_func(offset: int = 0):
 
 def get_target_loss_and_model(global_batch_shape: tuple, hidden_size: int, total_layers: int) -> float:  
     model = []
-    data = torch.ones(global_batch_shape, dtype=torch.float)
+    data = torch.ones(global_batch_shape, dtype=torch.double)
     for i in range(total_layers):
-        w = torch.ones((hidden_size, hidden_size)) * (i + 1.0) / weight_coeff
-        b = torch.ones(hidden_size)
+        w = torch.ones((hidden_size, hidden_size), dtype=torch.double) * (i + 1.0) / weight_coeff
+        b = torch.ones(hidden_size, dtype=torch.double)
 
         w.requires_grad_()
         b.requires_grad_()
@@ -93,7 +93,7 @@ class PipelineParallelForwardBackwardTestBase:
         if fwd_bwd_func == _forward_backward_pipelining_with_interleaving:
             self.assertIsNotNone(virtual_pipeline_model_parallel_size)
             self.assertGreater(virtual_pipeline_model_parallel_size, 1)
-        dtype_options = self.dtypes or [torch.float32] + _get_autocast_dtypes()
+        dtype_options = self.dtypes or [torch.float32, torch.double] + _get_autocast_dtypes()
 
         for dtype, deallocate_pipeline_outputs in itertools.product(
             dtype_options, self.deallocate_options,
@@ -135,7 +135,7 @@ class PipelineParallelForwardBackwardTestBase:
 
             batch = None
             if parallel_state.is_pipeline_first_stage():
-                batch = (torch.ones(global_batch_shape, dtype=torch.float).cuda(), )
+                batch = (torch.ones(global_batch_shape, dtype=dtype).cuda(), )
 
             model = build_model(
                 testing_utils.model_provider_func,
@@ -148,6 +148,7 @@ class PipelineParallelForwardBackwardTestBase:
             
             offset = pipeline_model_parallel_world_size if virtual_pipeline_model_parallel_size is not None else 0
             for idx, model_module in enumerate(model):
+                model_module = model_module.to(dtype)
                 model_module.apply(get_init_weights_func(idx*offset))
 
             _param_groups = _get_params_for_weight_decay_optimization(model)
@@ -155,25 +156,24 @@ class PipelineParallelForwardBackwardTestBase:
 
             pp_utils.update_num_microbatches(0)
             
-            with common_cuda.tf32_off():
-                loss = fwd_bwd_func(
-                    testing_utils.fwd_step_func,
-                    batch,
-                    model,
-                    forward_only=forward_only,
-                    # `tensor_shape` is the shape of micro batch.
-                    tensor_shape=(
-                        self.MICRO_BATCH_SIZE,
-                        self.HIDDEN_SIZE,
-                        self.HIDDEN_SIZE,
-                    ),
-                    dtype=dtype,
-                    async_comm=async_comm,
-                    grad_scaler=grad_scaler,
-                    deallocate_pipeline_output=deallocate_pipeline_outputs,
-                )
+            loss = fwd_bwd_func(
+                testing_utils.fwd_step_func,
+                batch,
+                model,
+                forward_only=forward_only,
+                # `tensor_shape` is the shape of micro batch.
+                tensor_shape=(
+                    self.MICRO_BATCH_SIZE,
+                    self.HIDDEN_SIZE,
+                    self.HIDDEN_SIZE,
+                ),
+                dtype=dtype,
+                async_comm=async_comm,
+                grad_scaler=grad_scaler,
+                deallocate_pipeline_output=deallocate_pipeline_outputs,
+            )
 
-            if dtype == torch.float32:
+            if dtype == torch.double:
                 hidden_size = self.HIDDEN_SIZE
                 microbatch_size = self.MICRO_BATCH_SIZE
                 total_layers = pipeline_model_parallel_world_size
