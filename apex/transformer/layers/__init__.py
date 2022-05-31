bcb41fbf2b7197442a6a370fbaff99625b0c7032
Masaki Kozuki <mkozuki@nvidia.com>
2022-05-31 10:03:28 -0700

    misc

--- apex/transformer/layers/__init__.py
+++ apex/transformer/layers/__init__.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
 from apex.transformer.layers.layer_norm import FastLayerNorm
 from apex.transformer.layers.layer_norm import FusedLayerNorm
 from apex.transformer.layers.layer_norm import MixedFusedLayerNorm
