cb6ac2da6103cd4b1d561c454000bc0a57fd548c
Aidyn-A <31858918+Aidyn-A@users.noreply.github.com>
2022-07-08 15:59:43 -0400

    remove redundant imports

--- tests/L0/run_transformer/test_pipeline_parallel_fwd_bwd_ucc_async.py
+++ tests/L0/run_transformer/test_pipeline_parallel_fwd_bwd_ucc_async.py
@@ -1,40 +1,15 @@
 import os
 import logging
-import itertools
-import re
-from typing import Optional, Tuple, List
-import unittest
 
 import torch
 from torch.testing._internal import common_utils
-from torch.testing._internal import common_cuda
 from torch.testing._internal import common_distributed
 
-from apex._autocast_utils import _get_autocast_dtypes
-from apex.transformer import parallel_state
-from apex.transformer.enums import ModelType
-from apex.transformer.pipeline_parallel import utils as pp_utils
-from apex.transformer.pipeline_parallel.schedules.common import (
-    FwdStepFunc,
-    build_model,
-    _get_params_for_weight_decay_optimization,
-)
-from apex.transformer.pipeline_parallel.schedules.fwd_bwd_no_pipelining import (
-    forward_backward_no_pipelining,
-)
-from apex.transformer.pipeline_parallel.schedules.fwd_bwd_pipelining_with_interleaving import (
-    _forward_backward_pipelining_with_interleaving,
-)
-from apex.transformer.pipeline_parallel.schedules.fwd_bwd_pipelining_without_interleaving import (
-    forward_backward_pipelining_without_interleaving,
-)
 from apex.transformer.testing.distributed_test_base import UccDistributedTestBase
 from apex.transformer.testing.distributed_test_base import HAS_TORCH_UCC
 from apex.transformer.testing.distributed_test_base import HAS_TORCH_UCC_COMPAT_NVIDIA_DRIVER
-from apex.transformer.testing import commons as testing_utils
 
 from test_pipeline_parallel_fwd_bwd import PipelineParallelForwardBackwardTestBase
-from test_pipeline_parallel_fwd_bwd import get_target_loss_and_model
 
 logging.getLogger("torch").setLevel(logging.WARNING)
 logging.getLogger("apex").setLevel(logging.WARNING)
