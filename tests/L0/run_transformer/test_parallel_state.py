d1b179b676011ed44ac095fc3479237122ee388c
Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>
2022-07-09 12:53:32 +0800

    fix semicolon bugs
    Co-authored-by: Masaki Kozuki <masaki.kozuki.2014@gmail.com>

--- tests/L0/run_transformer/test_parallel_state.py
+++ tests/L0/run_transformer/test_parallel_state.py
@@ -123,7 +123,7 @@ class ParallelStateTestBase:
         # relative position embedding groups check
         self.assertEqual(
            expected_pipeline_rank < pipeline_model_parallel_split_rank,
-           parallel_state.is_rank_in_encoder_relative_position_embedding_group();
+           parallel_state.is_rank_in_encoder_relative_position_embedding_group(),
         )
         self.assertEqual(
            expected_pipeline_rank >= pipeline_model_parallel_split_rank,
