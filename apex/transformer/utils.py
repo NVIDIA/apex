3c19f1061879394f28272a99a7ea26d58f72dace
Eric Harper <complex451@gmail.com>
2022-07-28 12:57:22 -0600

    Sequence parallel perf updates (#1437)
    * use _all_gather_base
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * use _reduce_scatter_base
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * remove torch empty in backward
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * check self.attn_mask_type
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * remove extra arg
    
    Signed-off-by: ericharper <complex451@gmail.com>
    
    * update get_tensor_shapes logic
    
    Signed-off-by: ericharper <complex451@gmail.com>

--- apex/transformer/utils.py
+++ apex/transformer/utils.py
@@ -40,8 +40,9 @@ def gather_split_1d_tensor(tensor):
         device=torch.cuda.current_device(),
         requires_grad=False,
     )
-    chunks = [gathered[i * numel : (i + 1) * numel] for i in range(world_size)]
-    torch.distributed.all_gather(
-        chunks, tensor, group=parallel_state.get_tensor_model_parallel_group()
-    )
+    torch.distributed._all_gather_base(
+        gathered,
+        tensor,
+        group=parallel_state.get_tensor_model_parallel_group()
+        )
     return gathered
