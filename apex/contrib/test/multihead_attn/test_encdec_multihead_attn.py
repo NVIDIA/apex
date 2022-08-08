51783cc7d81237bbade71fabe2c6ee6e824dee4d
hubertlu-tw <hubertlu@amd.com>
2022-08-08 18:16:50 +0000

    Revert code changes to mutltihead_attn tests

--- apex/contrib/test/multihead_attn/test_encdec_multihead_attn.py
+++ apex/contrib/test/multihead_attn/test_encdec_multihead_attn.py
@@ -40,37 +40,37 @@ class EncdecMultiheadAttnTest(unittest.TestCase):
                                              impl='fast')
         self.tst_layer.cuda().half()
         self.tst_layer.reset_parameters()
-        
+
         self.tst_inputs_q = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
                                         dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
         self.tst_inputs_k = torch.randn(self.seq_length, self.sequences, self.hidden_dim, 
                                         dtype=torch.float16, device=torch.device("cuda")).requires_grad_(True)
 
     def test_encdec_multihead_attn(self) :
+        grads         = torch.randn_like(self.tst_inputs_q)
+
         ref_outputs,_ = self.ref_layer.forward(self.ref_inputs_q,
                                                self.ref_inputs_k,
                                                self.ref_inputs_k,
-                                               key_padding_mask=None, 
-                                               need_weights=False, 
+                                               key_padding_mask=None,
+                                               need_weights=False,
                                                attn_mask=None,
                                                is_training=True)
 
-        tst_outputs,_ = self.tst_layer.forward(self.tst_inputs_q, 
-                                               self.tst_inputs_k, 
+        tst_outputs,_ = self.tst_layer.forward(self.tst_inputs_q,
                                                self.tst_inputs_k,
-                                               key_padding_mask=None, 
-                                               need_weights=False, 
+                                               self.tst_inputs_k,
+                                               key_padding_mask=None,
+                                               need_weights=False,
                                                attn_mask=None,
                                                is_training=True)
+
+        self.ref_inputs_q.backward(grads)
+        self.tst_inputs_q.backward(grads)
+
         self.assertTrue(torch.allclose(self.ref_inputs_q,  self.tst_inputs_q,  atol=1e-5, rtol=1e-5))
         self.assertTrue(torch.allclose(self.ref_inputs_k,  self.tst_inputs_k,  atol=1e-5, rtol=1e-5))
         self.assertTrue(torch.allclose(ref_outputs, tst_outputs, atol=1e-3, rtol=1e-3))
-
-        with torch.no_grad():
-            ref_grads         = torch.randn_like(ref_outputs)
-            tst_grads = ref_grads.clone()
-        ref_outputs.backward(ref_grads)
-        tst_outputs.backward(tst_grads)
         self.assertTrue(torch.allclose(self.ref_inputs_q.grad, self.tst_inputs_q.grad, atol=1e-3, rtol=1e-3))
     
     def test_encdec_multihead_attn_time_mask(self) :
