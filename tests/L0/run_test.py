cebbb04f9b712ea149d668eab621df690659700d
hubertlu-tw <hubertlu@amd.com>
2022-08-09 22:34:36 +0000

    Remove some comments in run_test.py

--- tests/L0/run_test.py
+++ tests/L0/run_test.py
@@ -1,38 +1,3 @@
-"""
-import unittest
-import sys
-
-from apex.testing.common_utils import TEST_WITH_ROCM
-from apex.testing.common_utils import SKIP_FLAKY_TEST
-
-test_dirs = ["run_amp", "run_fp16util", "run_optimizers", "run_fused_layer_norm", "run_pyprof_nvtx", "run_pyprof_data", "run_mlp"]
-
-ROCM_BLACKLIST = [
-    'run_pyprof_nvtx',
-    'run_pyprof_data',
-]
-
-runner = unittest.TextTestRunner(verbosity=2)
-
-errcode = 0
-
-for test_dir in test_dirs:
-    if (test_dir in ROCM_BLACKLIST) and TEST_WITH_ROCM and SKIP_FLAKY_TEST:
-        continue
-    suite = unittest.TestLoader().discover(test_dir)
-
-    print("\nExecuting tests from " + test_dir)
-
-    result = runner.run(suite)
-
-    if not result.wasSuccessful():
-        errcode = 1
-
-sys.exit(errcode)
-
-
-"""
-############################################################
 """L0 Tests Runner.
 
 How to run this script?
