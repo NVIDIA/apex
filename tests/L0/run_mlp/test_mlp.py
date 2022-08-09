8a8eb34f8521b45a3d954c10afe08ed728c5ba32
hubertlu-tw <hubertlu@amd.com>
2022-08-09 17:51:26 +0000

    Skip a flaky unit test

--- tests/L0/run_mlp/test_mlp.py
+++ tests/L0/run_mlp/test_mlp.py
@@ -134,6 +134,7 @@ class TestMLP(unittest.TestCase):
                 ref_mlp[0].bias.grad.detach().cpu().numpy(),
                 atol=1e-7, rtol=1e-5)
 
+    @skipFlakyTest
     def test_no_grad(self):
         mlp = MLP(mlp_sizes).cuda()
 
