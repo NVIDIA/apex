14e463cac47c90fdd16d58f570af3bdde0d04483
Masaki Kozuki <mkozuki@nvidia.com>
2022-09-09 17:00:58 -0700

    [contrib] xfail decorator to some of `transducer` tests (#1482)
    * mark xfail
    
    ```
    E       RuntimeError: one of the variables needed for gradient
    computation has been modified by an inplace operation:
    [torch.cuda.HalfTensor [4, 101, 25, 509]], which is output 0 of
    ReluBackward0, is at version 2; expected version 0 instead. Hint: enable
    anomaly detection to find the operation that failed to compute its
    gradient, with torch.autograd.set_detect_anomaly(True).
    ```
    
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>
    
    * remove unwanted decorator
    
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>
    
    * move reference impl to source
    
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>
    
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- apex/contrib/test/transducer/test_transducer_loss.py
+++ apex/contrib/test/transducer/test_transducer_loss.py
@@ -1,12 +1,14 @@
-import torch
 import unittest
+
+import torch
+
 from apex.contrib.transducer import TransducerLoss
-import transducer_ref
+from apex.contrib.transducer import _transducer_ref as transducer_ref
+
 
 class TransducerLossTest(unittest.TestCase):
     def setUp(self, seed=1234):
         torch.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
 
     def gen_input(self, scalar_t, for_vector_kernel):
         self.B = 5
@@ -18,10 +20,10 @@ class TransducerLossTest(unittest.TestCase):
         self.blank_idx = V - 1
         device = "cuda"
 
-        self.x_tst = torch.randn((self.B, T_max, U_max, V), dtype=scalar_t, requires_grad=True, 
+        self.x_tst = torch.randn((self.B, T_max, U_max, V), dtype=scalar_t, requires_grad=True,
                                     device=device)
         self.y = torch.randint(0, self.blank_idx, (self.B, U_max-1), dtype=torch.int, device=device)
-        self.f_len = torch.randint(T_min, T_max+1, (self.B,), dtype=torch.int, device=device) 
+        self.f_len = torch.randint(T_min, T_max+1, (self.B,), dtype=torch.int, device=device)
         self.y_len = torch.randint(U_min-1, U_max, (self.B,), dtype=torch.int, device=device)
         self.f_len[torch.randint(0, self.B, (1,)).item()] = T_max
         self.y_len[torch.randint(0, self.B, (1,)).item()] = U_max-1
@@ -31,11 +33,11 @@ class TransducerLossTest(unittest.TestCase):
         x_ref.requires_grad = True
         loss_grad = torch.ones(x_ref.size(0), dtype=x_ref.dtype, device=x_ref.device)/x_ref.size(0)
         _, _, self.grad_ref, self.loss_ref \
-            = transducer_ref.transducer_loss_reference( x=x_ref, 
-                                                        label=self.y, 
-                                                        f_len=self.f_len, 
-                                                        y_len=self.y_len, 
-                                                        blank_idx=self.blank_idx, 
+            = transducer_ref.transducer_loss_reference( x=x_ref,
+                                                        label=self.y,
+                                                        f_len=self.f_len,
+                                                        y_len=self.y_len,
+                                                        blank_idx=self.blank_idx,
                                                         loss_grad=loss_grad)
 
     def _pack(self, x):
@@ -50,7 +52,7 @@ class TransducerLossTest(unittest.TestCase):
         return x_packed, batch_offset
 
     def _unpack(self, x):
-        x_unpacked = torch.zeros(self.B, self.f_len.max(), self.y_len.max()+1, x.size(-1), 
+        x_unpacked = torch.zeros(self.B, self.f_len.max(), self.y_len.max()+1, x.size(-1),
                                     dtype=x.dtype, device=x.device)
         for b in range(self.B):
             my_batch_offset = 0 if b == 0 else self.batch_offset[b-1]
@@ -63,28 +65,28 @@ class TransducerLossTest(unittest.TestCase):
 
     def run_transducer_loss(self, scalar_t, fuse_softmax_backward, packed_input, for_vector_kernel):
         self.gen_input(scalar_t, for_vector_kernel)
-        my_loss = TransducerLoss(  fuse_softmax_backward=fuse_softmax_backward, 
-                                    packed_input=packed_input) 
+        my_loss = TransducerLoss(  fuse_softmax_backward=fuse_softmax_backward,
+                                    packed_input=packed_input)
         if not packed_input:
             loss_tst = my_loss( x=self.x_tst,
-                                label=self.y, 
-                                f_len=self.f_len, 
-                                y_len=self.y_len, 
+                                label=self.y,
+                                f_len=self.f_len,
+                                y_len=self.y_len,
                                 blank_idx=self.blank_idx)
-            loss_tst.mean().backward() 
+            loss_tst.mean().backward()
             grad_tst = self.x_tst.grad
         else:
             loss_tst = my_loss( x=self.x_tst_packed,
-                                label=self.y, 
-                                f_len=self.f_len, 
-                                y_len=self.y_len, 
+                                label=self.y,
+                                f_len=self.f_len,
+                                y_len=self.y_len,
                                 blank_idx=self.blank_idx,
-                                batch_offset=self.batch_offset, 
+                                batch_offset=self.batch_offset,
                                 max_f_len=max(self.f_len))
             loss_tst.mean().backward()
             grad_tst_packed = self.x_tst_packed.grad
             grad_tst = self._unpack(grad_tst_packed)
-        
+
         return loss_tst, grad_tst
 
     def test_transducer_loss_fp32(self):
@@ -126,7 +128,6 @@ class TransducerLossTest(unittest.TestCase):
                                                         for_vector_kernel=True)
         self.assertTrue(torch.allclose(self.loss_ref, loss_tst, atol=1e-5, rtol=1e-5))
         self.assertTrue(torch.allclose(self.grad_ref, grad_tst, atol=1e-4, rtol=1e-3))
-
 
 
 if __name__ == '__main__':
