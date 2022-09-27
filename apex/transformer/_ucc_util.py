6fae66be48769fe71c46aea914ff9f3c12c7d9b8
Fuzzkatt <zonghan2000@gmail.com>
2022-09-27 12:49:05 -0700

    small changes

--- apex/transformer/_ucc_util.py
+++ apex/transformer/_ucc_util.py
@@ -1,13 +1,10 @@
-from packaging.version import Version, parse
-
-import torch
 from torch import distributed as dist
-from torch.utils import collect_env
 
-_HAS_TORCH_UCC = None
-try:
-    import torch_ucc
-    _HAS_TORCH_UCC = True
-except ImportError:
-    _HAS_TORCH_UCC = False
-HAS_UCC = dist.is_ucc_available() or _HAS_TORCH_UCC
+HAS_UCC = dist.is_ucc_available()
+if not HAS_UCC:
+    try:
+        import torch_ucc
+        HAS_UCC = True
+    except ImportError:
+        HAS_UCC = False
+        
--- tests/L0/run_transformer/test_layers.py
+++ tests/L0/run_transformer/test_layers.py
@@ -277,19 +277,15 @@ class TensorParallelLayerTestBase:
     def test_row_parallel_linear_gradient_accumulation_fusion(self) -> None:
         self._row_parallel_linear_test_impl(True, False, False)
 
-    """
-    Fails in native ucc: reduction dtype: float16 not supported
-    Full error: 
-    ec_cuda_executor_persistent.c:39   cuda ec ERROR not supported reduction dtype: float16
-    reduce_scatter_knomial.c:174  TL_UCP ERROR failed to perform dt reduction
-    """
+    # Fails in native ucc: reduction dtype: float16 not supported
+    # Full error: 
+    # ec_cuda_executor_persistent.c:39   cuda ec ERROR not supported reduction dtype: float16
+    # reduce_scatter_knomial.c:174  TL_UCP ERROR failed to perform dt reduction
     def test_row_parallel_linear_gradient_accumulation_fusion_in_fp16(self) -> None:
         self._row_parallel_linear_test_impl(True, True, False)
 
-    """
-    Fails in native ucc: 
-    RuntimeError: ProcessGroup ucc does not support _reduce_scatter_base
-    """
+    # Fails in native ucc: 
+    # RuntimeError: ProcessGroup ucc does not support _reduce_scatter_base
     @unittest.skipIf(torch.cuda.device_count() < 2, "Sequence Parallel requires >=2 GPUs")
     def test_row_parallel_linear_sequence_parallel(self) -> None:
         self._row_parallel_linear_test_impl(False, False, True)
@@ -421,12 +417,10 @@ class TensorParallelLayerTestBase:
     def test_column_parallel_linear_gradient_accumulation_fusion(self):
         self._column_parallel_linear_test_impl(False, True, False, False)
 
-    """
-    Fails on native ucc: reduction dtype: float16 not supported
-    Full error: 
-    ec_cuda_executor_persistent.c:39   cuda ec ERROR not supported reduction dtype: float16
-    reduce_scatter_knomial.c:174  TL_UCP ERROR failed to perform dt reduction
-    """
+    # Fails on native ucc: reduction dtype: float16 not supported
+    # Full error: 
+    # ec_cuda_executor_persistent.c:39   cuda ec ERROR not supported reduction dtype: float16
+    # reduce_scatter_knomial.c:174  TL_UCP ERROR failed to perform dt reduction
     def test_column_parallel_linear_gradient_accumulation_fusion_in_fp16(self):
         self._column_parallel_linear_test_impl(False, True, True, False)
 
