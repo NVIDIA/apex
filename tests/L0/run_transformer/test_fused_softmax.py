b14e66fe3a7b1754b3de9b74e1efd19741bafd3a
Yu Yao <yuya@nvidia.com>
2022-11-16 09:10:46 -0800

    Assert grad equal in fused softmax test
    Signed-off-by: Yu Yao <yuya@nvidia.com>

--- tests/L0/run_transformer/test_fused_softmax.py
+++ tests/L0/run_transformer/test_fused_softmax.py
@@ -186,6 +186,7 @@ class TestFusedScaleMaskSoftmax(common_utils.TestCase):
                 g1 = g0.clone()
             expected.backward(g0)
             actual.backward(g1)
+            self.assertEqual(attention_scores_0.grad, attention_scores_1.grad, msg=msg)
 
     def test_autocast_fused_scale_softmax(self):
         for dtype in autocast_dtypes:
@@ -216,6 +217,7 @@ class TestFusedScaleMaskSoftmax(common_utils.TestCase):
                 g1 = g0.clone()
             expected.backward(g0)
             actual.backward(g1)
+            self.assertEqual(attention_scores_0.grad, attention_scores_1.grad, msg=msg)
 
     def test_fused_upper_triangle_mask_softmax(self):
         """
