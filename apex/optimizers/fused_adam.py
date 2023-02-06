b9343e6f46daeedf7be6000a688724f8415fdd4f
Nouamane Tazi <nouamane98@gmail.com>
2023-02-06 16:58:49 +0100

    Fix FusedAdam.zero_grad(set_to_none=True)

--- apex/optimizers/fused_adam.py
+++ apex/optimizers/fused_adam.py
@@ -94,8 +94,8 @@ class FusedAdam(torch.optim.Optimizer):
         else:
             raise RuntimeError('apex.optimizers.FusedAdam requires cuda extensions')
 
-    def zero_grad(self):
-        if self.set_grad_none:
+    def zero_grad(self, set_to_none=None):
+        if self.set_grad_none or set_to_none:
             for group in self.param_groups:
                 for p in group['params']:
                     p.grad = None
