50cbff71a2f8758db16346d8361bea2a7c6ef367
Nouamane Tazi <nouamane98@gmail.com>
2023-02-17 09:20:12 +0100

    set default to True

--- apex/optimizers/fused_adam.py
+++ apex/optimizers/fused_adam.py
@@ -94,7 +94,7 @@ class FusedAdam(torch.optim.Optimizer):
         else:
             raise RuntimeError('apex.optimizers.FusedAdam requires cuda extensions')
 
-    def zero_grad(self, set_to_none=None):
+    def zero_grad(self, set_to_none=True):
         if self.set_grad_none or set_to_none:
             for group in self.param_groups:
                 for p in group['params']:
