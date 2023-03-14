518b97cdf9f2e67cd3ff35fd6b3198f8c37bb23a
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2023-03-14 11:08:13 +0100

    It seems tensors have to be float

--- apex/optimizers/fused_adam.py
+++ apex/optimizers/fused_adam.py
@@ -142,13 +142,13 @@ class FusedAdam(torch.optim.Optimizer):
                     state['exp_avg_sq'] = torch.zeros_like(p.data)
                     # Backward compatibility, we
                     assert 'step' not in group
-                    state['step'] = 1 if not self.capturable else torch.tensor([1], dtype=torch.int, device=device)
+                    state['step'] = torch.tensor(1.) if not self.capturable else torch.tensor([1], dtype=torch.float, device=device)
                 else:
                     # Backward compatibility: we used to assume that `step` was the same across group
                     if 'step' in group:
                         assert 'step' not in state
                         state['step'] = group['step']
-                    state['step'] += 1 if not self.capturable else (self._dummy_overflow_buf != 1).to(torch.int)
+                    state['step'] += 1.0 if not self.capturable else (self._dummy_overflow_buf != 1).to(torch.float)
 
                 if p.dtype == torch.float16:
                     g_16.append(p.grad.data)
