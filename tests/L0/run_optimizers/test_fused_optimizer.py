2787a4429da80b8a5070e895fcdfd491ee842ad3
JimmyZhang12 <67203904+JimmyZhang12@users.noreply.github.com>
2023-03-17 17:08:37 -0700

    Frozen model unit test

--- tests/L0/run_optimizers/test_fused_optimizer.py
+++ tests/L0/run_optimizers/test_fused_optimizer.py
@@ -130,7 +130,7 @@ class TestFusedAdam(TestFusedOptimizer):
             max_abs_diff, max_rel_diff = self.get_max_diff(ref_param, tst_param)
             self.assertLessEqual(max_abs_diff, self.max_abs_diff)
             self.assertLessEqual(max_rel_diff, self.max_rel_diff)
-
+            
     @unittest.skip('No longer support fuse scaling')
     def test_scale(self):
         nelem = 278011
@@ -189,8 +189,29 @@ class TestFusedAdam(TestFusedOptimizer):
 
             self.assertLessEqual(max_abs_diff, self.max_abs_diff)
             self.assertLessEqual(max_rel_diff, self.max_rel_diff)
+            
+    def test_frozen_model(self):
+        nelem = 1
+        adam_option = {'lr':0.01, 'betas':(0.6, 0.9), 'eps':3e-06,
+            'weight_decay':0, 'amsgrad':False}
 
+        tensor = torch.rand(nelem, dtype=torch.float, device='cuda')
+        ref_param, tst_param, ref_optim, tst_optim = \
+            self.gen_param_optim([tensor], adam_option)
 
+        #Add an empty param group which may occur for pipeline parallel p-tuning
+        tst_optim.add_param_group({"params": []})
+
+        for i in range(self.iters):
+            self.gen_grad(ref_param, tst_param)
+            ref_optim.step()
+            tst_optim.step()
+            max_abs_diff, max_rel_diff = self.get_max_diff(ref_param, tst_param)
+
+            self.assertLessEqual(max_abs_diff, self.max_abs_diff)
+            self.assertLessEqual(max_rel_diff, self.max_rel_diff)
+
+            
 class TestFusedAdagrad(TestFusedOptimizer):
     def __init__(self, *args, **kwargs):
         super(TestFusedAdagrad, self).__init__(*args, **kwargs)
