Fix potential issue with gradient unscaling

tests/L0/run_optimizers/test_adam.py
@@ -68,7 +68,7 @@ class AdamTest(unittest.TestCase):
 
     def testGradScaler(self):
         params_ = [p for p in self.model_.parameters() if p.requires_grad]
-        optimizer_ = apex.optimizers.FusedAdam(params_, lr=self.lr, capturable=False, use_master=True)
+        optimizer_ = apex.optimizers.FusedAdam(params_, lr=self.lr, capturable=False)
         scaler = torch.cuda.amp.GradScaler(enabled=True)
         scaler_ = torch.cuda.amp.GradScaler(enabled=True)
 
@@ -154,7 +154,7 @@ class AdamTest(unittest.TestCase):
     
     def testNative(self):
         params_ = [p for p in self.model_.parameters() if p.requires_grad]
-        optimizer_ = apex.optimizers.FusedAdam(params_, lr=self.lr, capturable=False, use_master=True)
+        optimizer_ = apex.optimizers.FusedAdam(params_, lr=self.lr, capturable=False)
 
         for i in range(100):
             x = torch.rand([32, 1, 28, 28]).cuda().to(memory_format=torch.channels_last)