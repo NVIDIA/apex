6bd01c4b99a84648ad5e5238a959735e6936c813
Jaemin Choi <minitu77@gmail.com>
2023-04-13 21:37:02 -0700

    Add support for FP32 master weights in FusedAdam optimizer (#1625)
    * Cherry pick changes to ConvScaleBiasReLU fusion
    
    * Fix testbench
    
    * Add missing conv_cscale_cbias_relu_forward
    
    * Fix bug in setOperationGraph
    
    * Remove manual cuDNN heuristics knobs
    
    * Use torch.testing.assert_close for tensor comparison
    
    * Return at::Tensor instead of vector, add debug msg
    
    * Start making changes
    
    * Changes
    
    * Update
    
    * Probably all necessary changes, need to test compilation
    
    * Fix bug
    
    * Fix bug
    
    * Fix bug
    
    * Change implementation to separately maintain master weights
    
    * Update test
    
    * Fix test
    
    * Update test
    
    * Fix potential issue with gradient unscaling
    
    * Write out unscaled gradients
    
    * Debugging test
    
    * Add static casts
    
    * Test
    
    * Test
    
    * Revert test
    
    * Add debugging prints
    
    * Fix bug
    
    * Make m and v FP32
    
    * Fix compilation bug
    
    * m and v
    
    * Revert test
    
    * Remove debug prints
    
    * Remove print
    
    * Remove assert
    
    * Cleanup
    
    * Cleanup
    
    * Update test
    
    * Remove float conversions for m and v
    
    * Fix typo
    
    * Fix gradient write
    
    * Master params for full precision
    
    * Fix typo
    
    * Add non-capturable master version
    
    * Remove RuntimeError
    
    * Fix bug
    
    * Move where unscaling factor is obtained
    
    * Update non-capturable with use_master
    
    * LR needs to be on GPU for use_master
    
    * Use master only with capturable, follow FusedLambMP
    
    * Create master params in optimizer init
    
    * Add restoration of master weights
    
    * Include master weights in state_dict
    
    * Add missing modules
    
    * Remove param_groups_master_init
    
    * Update docstring
    
    * Create/recreate master weights in optimizer step
    
    * Rename use_master to master_weights
    
    * Have training script back up and restore master params
    
    * Remove extra line breaks
    
    * `use_master` -> `master_weights`
    
    ---------
    
    Co-authored-by: Jaemin Choi <jaeminc@nvidia.com>
    Co-authored-by: root <root@cl1-2546.nvidia.com>
    Co-authored-by: root <root@cl1-2582.nvidia.com>
    Co-authored-by: Masaki Kozuki <mkozuki@nvidia.com>

--- tests/L0/run_optimizers/test_adam.py
+++ tests/L0/run_optimizers/test_adam.py
@@ -147,7 +147,54 @@ class AdamTest(unittest.TestCase):
             optimizer_.zero_grad()
 
             self.model_.load_state_dict(copy.deepcopy(self.model.state_dict()))
-    
+
+    def testGradScalerCapturableMaster(self):
+        # Cast conv layers to FP16
+        for m in self.model_.modules():
+            if m.__class__ in [torch.nn.Conv2d]:
+                m.half()
+        params_ = [p for p in self.model_.parameters() if p.requires_grad]
+        optimizer_ = apex.optimizers.FusedAdam(params_, lr=self.lr, capturable=True, master_weights=True)
+        scaler = torch.cuda.amp.GradScaler(enabled=True)
+        scaler_ = torch.cuda.amp.GradScaler(enabled=True)
+
+        for i in range(100):
+            x = torch.rand([32, 1, 28, 28]).cuda().to(memory_format=torch.channels_last)
+            x_ = x.clone()
+            gt = torch.rand([32, 10]).cuda()
+            gt_ = gt.clone()
+
+            # Reference
+            with torch.cuda.amp.autocast(enabled=True):
+                y = self.model(x)
+                loss = ((gt - y) ** 2).mean()
+
+            scaler.scale(loss).backward()
+            scaler.step(self.optimizer)
+            scaler.update()
+
+            # DUT
+            with torch.cuda.amp.autocast(enabled=True):
+                y = self.model_(x)
+                loss_ = ((gt_ - y) ** 2).mean()
+
+            scaler_.scale(loss_).backward()
+            scaler_.step(optimizer_)
+            scaler_.update()
+
+            for module in zip(self.model.modules(), self.model_.modules()):
+                m = module[0]
+                m_ = module[1]
+                if isinstance(m, nn.Conv2d) or isinstance(m_, nn.Linear):
+                    torch.testing.assert_close(m.weight, m_.weight.float(), atol=1e-3, rtol=1e-3, equal_nan=True)
+                    torch.testing.assert_close(m.weight.grad, m_.weight.grad.float(), atol=1e-3, rtol=1e-3, equal_nan=True)
+
+            # Init for next iteration
+            self.optimizer.zero_grad()
+            optimizer_.zero_grad()
+
+            self.model_.load_state_dict(copy.deepcopy(self.model.state_dict()))
+
     def testNative(self):
         params_ = [p for p in self.model_.parameters() if p.requires_grad]
         optimizer_ = apex.optimizers.FusedAdam(params_, lr=self.lr, capturable=False)
