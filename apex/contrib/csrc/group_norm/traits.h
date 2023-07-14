daeeefca8352991ad53996a20da2a71c3ac7844b
Wil Kong <alpha0422@gmail.com>
2023-07-14 09:24:48 +0800

    Update license in group norm.

--- apex/contrib/csrc/group_norm/traits.h
+++ apex/contrib/csrc/group_norm/traits.h
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright (c) 2011-2021, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2023, NVIDIA CORPORATION.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are not permit-
  * ted.
--- apex/contrib/group_norm/group_norm.py
+++ apex/contrib/group_norm/group_norm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Copyright (c) 2011-2022, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2011-2023, NVIDIA CORPORATION.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are not permit-
 # ted.
