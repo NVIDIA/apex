6847532534e5307b66916d8e85296a8308ec6fc1
Feiwen Zhu <mzhu@nvidia.com>
2023-10-14 00:01:16 +0800

    refine openfold triton kernel test

--- apex/contrib/test/openfold_triton/test_openfold_mha.py
+++ apex/contrib/test/openfold_triton/test_openfold_mha.py
@@ -1,8 +1,14 @@
 import math
+import random
 from typing import Optional
 import torch
+import unittest
 
-from apex.contrib.openfold_triton import AttnTri as openfold_attention_triton
+SKIP_TEST = None
+try:
+    from apex.contrib.openfold_triton import AttnTri as openfold_attention_triton
+except ImportError as e:
+    SKIP_TEST = e
 
 
 def openfold_attention_eager(
@@ -44,68 +50,77 @@ def openfold_attention_eager(
     return a
 
 
-def test_openfold_triton_mha(Z, H, N_CTX, D_HEAD, dtype=torch.float16):
-    torch.manual_seed(20)
-    One = 1
-    q = (
-        torch.empty((One, Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
-        .normal_(mean=0.1, std=0.2)
-        .requires_grad_()
-    )
-    k = (
-        torch.empty((One, Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
-        .normal_(mean=0.4, std=0.2)
-        .requires_grad_()
-    )
-    v = (
-        torch.empty((One, Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
-        .normal_(mean=0.3, std=0.2)
-        .requires_grad_()
-    )
-    bias = (
-        torch.empty((One, One, H, N_CTX, N_CTX), dtype=dtype, device="cuda")
-        .normal_(mean=0.2, std=0.2)
-        .requires_grad_()
-    )
-    mask = (
-        torch.empty((One, N_CTX, One, One, N_CTX), device="cuda").normal_(
-            mean=0, std=0.5
+@unittest.skipIf(SKIP_TEST, f"{SKIP_TEST}")
+class OpenfoldMhaTest(unittest.TestCase):
+    def setUp(self, seed=1234):
+        super().setUp()
+        random.seed(seed)
+        torch.manual_seed(seed)
+
+    # representative workload in openfold
+    def test_openfold_triton_mha(
+        self, Z=256, H=4, N_CTX=256, D_HEAD=32, dtype=torch.float16
+    ):
+        One = 1
+        q = (
+            torch.empty((One, Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
+            .normal_(mean=0.1, std=0.2)
+            .requires_grad_()
         )
-        > 0
-    )
-    mask = mask.to(device=torch.device("cuda"), dtype=dtype).requires_grad_(False)
+        k = (
+            torch.empty((One, Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
+            .normal_(mean=0.4, std=0.2)
+            .requires_grad_()
+        )
+        v = (
+            torch.empty((One, Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
+            .normal_(mean=0.3, std=0.2)
+            .requires_grad_()
+        )
+        bias = (
+            torch.empty((One, One, H, N_CTX, N_CTX), dtype=dtype, device="cuda")
+            .normal_(mean=0.2, std=0.2)
+            .requires_grad_()
+        )
+        mask = (
+            torch.empty((One, N_CTX, One, One, N_CTX), device="cuda").normal_(
+                mean=0, std=0.5
+            )
+            > 0
+        )
+        mask = mask.to(device=torch.device("cuda"), dtype=dtype).requires_grad_(False)
 
-    dout = torch.randn_like(q)
-    inf = 1e9
+        dout = torch.randn_like(q)
+        inf = 1e9
 
-    # reference implementation
-    ref_out = openfold_attention_eager(q, k, v, mask, bias, inf)
-    ref_out.backward(dout)
+        # reference implementation
+        ref_out = openfold_attention_eager(q, k, v, mask, bias, inf)
+        ref_out.backward(dout)
 
-    ref_dv, v.grad = v.grad.clone(), None
-    ref_dk, k.grad = k.grad.clone(), None
-    ref_dq, q.grad = q.grad.clone(), None
-    ref_dbias, bias.grad = bias.grad.clone(), None
+        ref_dv, v.grad = v.grad.clone(), None
+        ref_dk, k.grad = k.grad.clone(), None
+        ref_dq, q.grad = q.grad.clone(), None
+        ref_dbias, bias.grad = bias.grad.clone(), None
 
-    # triton implementation
-    tri_out = openfold_attention_triton(
-        q, k, v, mask, bias, inf, torch.is_grad_enabled()
-    )
-    tri_out.backward(dout)
+        # triton implementation
+        tri_out = openfold_attention_triton(
+            q, k, v, mask, bias, inf, torch.is_grad_enabled()
+        )
+        tri_out.backward(dout)
 
-    tri_dv, v.grad = v.grad.clone(), None
-    tri_dk, k.grad = k.grad.clone(), None
-    tri_dq, q.grad = q.grad.clone(), None
-    tri_dbias, bias.grad = bias.grad.clone(), None
+        tri_dv, v.grad = v.grad.clone(), None
+        tri_dk, k.grad = k.grad.clone(), None
+        tri_dq, q.grad = q.grad.clone(), None
+        tri_dbias, bias.grad = bias.grad.clone(), None
 
-    # check results
-    assert torch.allclose(ref_out, tri_out, atol=1e-2, rtol=0)
-    assert torch.allclose(ref_dv, tri_dv, atol=1e-2, rtol=0)
-    assert torch.allclose(ref_dk, tri_dk, atol=1e-2, rtol=0)
-    assert torch.allclose(ref_dq, tri_dq, atol=1e-2, rtol=0)
-    assert torch.allclose(ref_dbias, tri_dbias, atol=1e-2, rtol=0)
+        # check results
+        torch.testing.assert_close(ref_out, tri_out, atol=1e-2, rtol=0)
+        torch.testing.assert_close(ref_dv, tri_dv, atol=1e-2, rtol=0)
+        torch.testing.assert_close(ref_dk, tri_dk, atol=1e-2, rtol=0)
+        torch.testing.assert_close(ref_dq, tri_dq, atol=1e-2, rtol=0)
+        torch.testing.assert_close(ref_dbias, tri_dbias, atol=1e-2, rtol=0)
 
 
 if __name__ == "__main__":
-    # representative workload in openfold
-    test_openfold_triton_mha(Z=256, H=4, N_CTX=256, D_HEAD=32, dtype=torch.bfloat16)
+    unittest.main()
+
