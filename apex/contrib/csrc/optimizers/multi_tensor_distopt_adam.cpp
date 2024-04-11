10af2d00b1060d5950b612de6c41572046c86248
Wil Kong <alpha0422@gmail.com>
2024-04-11 08:55:42 +0800

    Support CUDA graph for distributed fused adam.

--- apex/contrib/csrc/optimizers/multi_tensor_distopt_adam.cpp
+++ apex/contrib/csrc/optimizers/multi_tensor_distopt_adam.cpp
@@ -14,6 +14,20 @@ void multi_tensor_fused_adam_cuda(
   int bias_correction,
   float weight_decay);
 
+void multi_tensor_fused_adam_capturable_cuda(
+  int chunk_size,
+  at::Tensor noop_flag,
+  std::vector<std::vector<at::Tensor>> tensor_lists,
+  at::Tensor grad_scale,
+  at::Tensor lr,
+  float beta1,
+  float beta2,
+  float eps,
+  at::Tensor step,
+  int mode,
+  int bias_correction,
+  float weight_decay);
+
 void multi_tensor_fused_adam_with_param_remainders_cuda(
   int chunk_size,
   at::Tensor noop_flag,
@@ -33,6 +47,10 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
         &multi_tensor_fused_adam_cuda,
         "CUDA kernels for multi-tensor Adam, "
         "with param copy");
+  m.def("multi_tensor_fused_adam_capturable",
+        &multi_tensor_fused_adam_capturable_cuda,
+        "CUDA kernels for multi-tensor Adam, "
+        "with param copy, capturable for CUDA graph");
   m.def("multi_tensor_fused_adam_with_param_remainders",
         &multi_tensor_fused_adam_with_param_remainders_cuda,
         "CUDA kernel for multi-tensor Adam, "
