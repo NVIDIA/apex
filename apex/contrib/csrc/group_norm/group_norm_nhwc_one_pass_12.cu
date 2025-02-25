c0a7ee6ef16e7565d2926948670a5b2632899d74
Tailing Yuan <yuantailing@gmail.com>
2025-02-26 01:26:36 +0800

    Update copyright year
    Co-authored-by: Masaki Kozuki <mkozuki@nvidia.com>

--- apex/contrib/csrc/group_norm/group_norm_nhwc_one_pass_12.cu
+++ apex/contrib/csrc/group_norm/group_norm_nhwc_one_pass_12.cu
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright (c) 2011-2023, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2025, NVIDIA CORPORATION.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are not permit-
  * ted.
