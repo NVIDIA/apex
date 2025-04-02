74ac4e58d6dd896bf53731e5ccd261e7a86d0cc3
Wil Kong <alpha0422@gmail.com>
2025-04-02 14:39:59 +0800

    torch.compile pass for replacing layer norm with cudnn version.

---
+++ apex/contrib/torchsched/passes/__init__.py
@@ -0,0 +1,7 @@
+"""Customized compiler passes."""
+
+from __future__ import annotations
+
+from apex.contrib.torchsched.passes.pre_grad_passes import pre_grad_custom_pass
+
+__all__ = ["pre_grad_custom_pass"]
