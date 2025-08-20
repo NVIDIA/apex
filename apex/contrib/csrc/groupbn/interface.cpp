f1804bb31524c95547b444aa2d6d66f8fce6f074
Masaki Kozuki <mkozuki@nvidia.com>
2025-08-20 05:34:26 +0000

    contrib groupbn include fix
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- apex/contrib/csrc/groupbn/interface.cpp
+++ apex/contrib/csrc/groupbn/interface.cpp
@@ -7,9 +7,6 @@
 #include <ATen/ArrayRef.h>
 #include <ATen/ScalarType.h>
 #include "ATen/Scalar.h"
-#ifndef VERSION_GE_1_1
-#include "ATen/Type.h"
-#endif
 #include "ATen/Tensor.h"
 #include "ATen/Storage.h"
 #include "ATen/Generator.h"
