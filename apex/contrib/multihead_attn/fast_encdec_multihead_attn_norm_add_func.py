9c0401fae7216ed75dd3cd58c123362cea3b8bca
Aidyn-A <aidyn.b.aitzhan@gmail.com>
2025-09-19 17:06:56 +0400

    deprecate apex.contrib.fmha and apex.contrib.multihead_attn

--- apex/contrib/multihead_attn/fast_encdec_multihead_attn_norm_add_func.py
+++ apex/contrib/multihead_attn/fast_encdec_multihead_attn_norm_add_func.py
@@ -27,6 +27,13 @@ class FastEncdecAttnNormAddFunc(torch.autograd.Function):
         pad_mask,
         dropout_prob,
     ):
+        from apex import deprecated_warning
+        deprecated_warning(
+            "`apex.contrib.multihead_attn` is deprecated and will be removed in March 2026. "
+            "We encourage you to migrate to PyTorch native MultiheadAttention"
+            "The documentation is available in https://docs.pytorch.org/docs/main/generated/torch.nn.MultiheadAttention.html"
+        )
+
         heads_t = torch.tensor([heads])
         dropout_prob_t = torch.tensor([dropout_prob])
         null_tensor = torch.tensor([])
