c364a446b7d48a67ad349b0cab0ad5c88e0b0963
syedshazli <syed.abdu1818@gmail.com>
2025-12-23 18:18:57 -0500

    remove transformer folder

--- apex/transformer/_data/__init__.py
+++
@@ -1,8 +0,0 @@
-from apex.transformer._data._batchsampler import MegatronPretrainingRandomSampler
-from apex.transformer._data._batchsampler import MegatronPretrainingSampler
-
-
-__all__ = [
-    "MegatronPretrainingRandomSampler",
-    "MegatronPretrainingSampler",
-]
